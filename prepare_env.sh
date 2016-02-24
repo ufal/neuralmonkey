35492f4ba812453ce0497adea97ad6a8d387e7ed
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-02-24 16:41:20 +0100

    Adding jwordsplitter

--- prepare_env.sh
+++ prepare_env.sh
@@ -7,5 +7,9 @@ fi
 source tf/bin/activate
 
 pip install --upgrade pip
-pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp27-none-linux_x86_64.whl
+pip install ipdb
+pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp27-none-linux_x86_64.whl
 
+git clone https://github.com/danielnaber/jwordsplitter
+cd jwordsplitter
+./build.sh
