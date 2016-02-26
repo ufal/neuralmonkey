639e7988debd0ca040b3452f43bbb5a7e0b00e45
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-02-26 17:09:38 +0100

    Adding the decompounder in Java to tokenization

--- prepare_env.sh
+++ prepare_env.sh
@@ -10,6 +10,10 @@ pip install --upgrade pip
 pip install ipdb
 pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp27-none-linux_x86_64.whl
 
+cd tf
 git clone https://github.com/danielnaber/jwordsplitter
 cd jwordsplitter
 ./build.sh
+cd ../..
+
+pip install --upgrade javabridge
--- tokenize_data.py
+++ tokenize_data.py
@@ -14,6 +14,21 @@ if __name__ == "__main__":
     sys.stdin = codecs.getreader('utf-8')(sys.stdin)
     sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
-    for line in sys.stdin:
-        print ' '.join(word_tokenize(line, language=args.language))
+    if args.language == "german":
+        import javabridge
+        javabridge.start_vm(class_path=["tf/jwordsplitter/target/jwordsplitter-4.2-SNAPSHOT.jar"])
+        java_instance = javabridge.make_instance("de/danielnaber/jwordsplitter/GermanWordSplitter", "(Z)V", True)
+        decompounder = javabridge.JWrapper(java_instance)
 
+    for line in sys.stdin:
+        tokenized = word_tokenize(line, language=args.language)
+
+        if args.language == "german":
+            for i, token in enumerate(tokenized):
+                decompounded = decompounder.splitWord(token)
+                if decompounded.size() >= 2:
+                    tokenized[i] = \
+                        "#".join([decompounded.get(j) for j in range(decompounded.size())])
+
+        print ' '.join(tokenized)
+    javabridge.kill_vm()
