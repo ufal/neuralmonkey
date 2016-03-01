8ddf25c006d850492269a0c185d0e639fd3e2e3c
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-03-01 19:27:54 +0100

    Tokenization

--- tokenize_data.py
+++ tokenize_data.py
@@ -1,34 +1,108 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import sys, codecs, argparse
 from nltk import word_tokenize
+import javabridge
+import gc
+import regex as re
+from termcolor import cprint
+
+def get_decompounder():
+    """
+    Restarts the JVM with the decompounder. It is necessary once in a while.
+    """
+    javabridge.start_vm(class_path=["tf/jwordsplitter/target/jwordsplitter-4.2-SNAPSHOT.jar"])
+    java_instance = javabridge.make_instance("de/danielnaber/jwordsplitter/GermanWordSplitter", "(Z)V", True)
+    decompounder = javabridge.JWrapper(java_instance)
+    return decompounder
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--language", help="Lanuage the text is in.")
+    parser = argparse.ArgumentParser(description="Tokenizes and decompounds the text on stdin.")
+    parser.add_argument("--language", required=True, help="Lanuage the text is in.")
     args = parser.parse_args()
 
     if args.language not in set(['english', 'german']):
-        raise Exception("Language must be 'englis' or 'german', not '{}'.".format(args.language))
+        raise Exception("Language must be 'english' or 'german', not '{}'.".format(args.language))
 
     sys.stdin = codecs.getreader('utf-8')(sys.stdin)
     sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+    sys.stderr = codecs.getwriter('utf-8')(sys.stderr)
 
     if args.language == "german":
-        import javabridge
-        javabridge.start_vm(class_path=["tf/jwordsplitter/target/jwordsplitter-4.2-SNAPSHOT.jar"])
-        java_instance = javabridge.make_instance("de/danielnaber/jwordsplitter/GermanWordSplitter", "(Z)V", True)
-        decompounder = javabridge.JWrapper(java_instance)
+        decompounder = get_decompounder()
+        decompounded_count = 0
 
-    for line in sys.stdin:
-        tokenized = word_tokenize(line, language=args.language)
+    for ln, line in enumerate(sys.stdin):
+        line = re.sub(ur"[[:space:]]+", " ", line.rstrip())
+        line = re.sub(ur"([[:alpha:]ß])-([ [:punct:]])", "\g<1>\g<2>", line, re.UNICODE)
+        line = re.sub(ur" - ", u" – ", line)
+
+        def normalize_quotes(token):
+            token = re.sub(r"-$", u'', token)
+            token = re.sub(r"``", u'\u201c', token)
+            token = re.sub(r"''", u'\u201d', token)
+            return token
+
+        tokenized = [normalize_quotes(t) for t in word_tokenize(line, language=args.language)]
 
         if args.language == "german":
             for i, token in enumerate(tokenized):
+                decompounded_count += 1
                 decompounded = decompounder.splitWord(token)
                 if decompounded.size() >= 2:
                     tokenized[i] = \
                         "#".join([decompounded.get(j) for j in range(decompounded.size())])
+                del decompounded
 
-        print ' '.join(tokenized)
+                if token.endswith("s") and not tokenized[i].endswith("s"):
+                    tokenized[i] += "s"
+
+                # we need to manually garbage collect because of Java Heap Space
+                if decompounded_count % 300 == 0:
+                    gc.collect()
+
+        tokenized_string = ' '.join(tokenized)
+
+        # Now put special character for spaces introduced by the tokenizer
+        original_i = 0
+        tokenized_chars_result = []
+        for tokenized_i, char in enumerate(tokenized_string):
+            #print u"pair '{}' ({}) and '{}' ({})".format(char, ord(char), line[original_i], ord(line[original_i]))
+            if char == line[original_i] or (char == " " and ord(line[original_i]) == 160):
+                tokenized_chars_result.append(char)
+                original_i += 1
+                #print u"same characters {}".format(char)
+            elif line[original_i] == '"' and (char == u'\u201c' or char == u'\u201d'):
+                original_i += 1
+                #print u"quotation mark {}".format(char)
+            elif char == " ":
+                tokenized_chars_result.append("@")
+                #print "space added by tokenizer"
+            elif char == "#":
+                if line[original_i] == "-":
+                    tokenized_chars_result.append("-")
+                    original_i += 1
+                else:
+                    if args.language == 'german' and line[original_i] == "s" and line[original_i + 1] == tokenized_string[tokenized_i + 1]:
+                        original_i += 1
+                        tokenized_chars_result.append("$")
+                        #print "decompounded with inserted s"
+                    if args.language == 'german' and line[original_i] == "s" and line[original_i + 1] == "-":
+                        original_i += 2
+                        tokenized_chars_result.append("-")
+                    else:
+                        #print "decompompounded"
+                        tokenized_chars_result.append("#")
+            else:
+                print ""
+                print "Error on line {}".format(ln)
+                cprint(u"tokenized on index {}: \"{}\", original on index {}: \"{}\"".format(tokenized_i, char, original_i, line[original_i]), 'yellow')
+                cprint(line, 'red')
+                cprint(tokenized_string, 'red')
+                javabridge.kill_vm()
+                exit()
+
+        print "".join(tokenized_chars_result)
+
     javabridge.kill_vm()
--- vocabulary.py
+++ vocabulary.py
@@ -71,14 +71,14 @@ class Vocabulary(object):
         return word_indices, weights
 
     def vectors_to_sentences(self, vectors):
-        sentences = [["<s>"] for _ in range(vectors[0].shape[0])]
+        sentences = [[] for _ in range(vectors[0].shape[0])]
 
         for vec in vectors:
             for sentence, word_i in zip(sentences, vec):
                 if sentence[-1] != "</s>":
                     sentence.append(self.index_to_word[word_i])
 
-        return [s[1:-1] for s in sentences]
+        return [s[:-1] for s in sentences]
 
 
 
