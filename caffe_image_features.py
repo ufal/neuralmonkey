41b8466c797bfb36ec4abf67afe4c82c44cffbe8
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-04-06 00:47:58 +0200

    Imagenet features by batches

--- caffe_image_features.py
+++ caffe_image_features.py
@@ -6,7 +6,8 @@ Does the Image feature extraction by calling directly Caffe.
 Based on tutorial http://www.marekrei.com/blog/transforming-images-to-feature-vectors/
 
 """
-import sys
+import sys, os
+os.environ['GLOG_minloglevel'] = '4'
 sys.path.append("caffe/python")
 import os, argparse, caffe
 import numpy as np
@@ -23,10 +24,16 @@ if __name__ == "__main__":
     parser.add_argument("--output-file", type=argparse.FileType('wb'), required=True)
     args = parser.parse_args()
 
+    log("Loading the ImageNet labels")
+    with open("imagenet_synset_words.txt") as f:
+         labels = f.readlines()
+
+
     # Setting this to CPU, but feel free to use GPU if you have CUDA installed
     caffe.set_mode_cpu()
 
     # Loading the Caffe model, setting preprocessing parameters
+    log("Model starts loading")
     net = caffe.Classifier(args.model_prototxt, args.model_parameters,
                            mean=np.load(args.img_mean).mean(1).mean(1),
                            channel_swap=(2,1,0),
@@ -37,15 +44,31 @@ if __name__ == "__main__":
 
     # Processing one image at a time, printint predictions and writing the vector to a file
     data = []
+    input_images = []
+    paths = []
+
+    def process_batch(input_images, paths):
+        prediction = net.predict(input_images, oversample=False)
+        f_output = net.blobs[args.feature_layer].data.transpose((0,2,3,1)).copy()
+        for img, p in zip(paths, prediction):
+            print os.path.basename(img), ' : ' , labels[p.argmax()].strip() , ' (', p[p.argmax()] , ')'
+        data.append(f_output)
+
     for i, image_path in enumerate(args.image_list):
         image_path = image_path.strip()
-        input_image = caffe.io.load_image(os.path.join(args.image_directory, image_path))
-        prediction = net.predict([input_image], oversample=False)
-        f_output = net.blobs[args.feature_layer].data[0].transpose((1,2,0)).copy()
-        data.append(np.expand_dims(f_output, axis=0))
+        paths.append(image_path)
+        input_images.append(caffe.io.load_image(os.path.join(args.image_directory, image_path)))
+
+        if len(input_images) >= 10:
+            process_batch(input_images, paths)
+            input_images = []
+            paths = []
+
         if i % 99 == 0:
             log("Processed {} images.".format(i + 1))
 
+    process_batch(input_images, paths)
+
     log("All images processed.")
-    np.save(args.output_file, np.concatenate(data))
-    log("Featurs saved. Shape: {}".format(data[0].shape))
+    np.save(args.output_file, np.concatenate(data, axis=0))
+    log("Features saved. Shape: {}".format(data[0][0].shape))
