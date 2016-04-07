f1877dda5fa8e5ade2d147bd39a4137114edb721
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-04-07 21:37:36 +0200

    Pronouns and first dealing with compounds

--- language_utils.py
+++ language_utils.py
@@ -22,6 +22,11 @@ for cont, (prep, article) in zip(contractions, uncontracted_forms):
     contract[article][prep] = cont
 
 
+ein_type_pronouns = re.compile("^(ein|[mdsk]ein|ihr|unser|euer|Ihr)(e|es|er|em|en)$")
+der_type_pronouns = re.compile("^(dies|welch|jed|all)(e|es|er|em|en)$")
+
+
+
 class GermanPreprocessor(object):
     def __init__(self, compounding=True, contracting=True, pronouns=True):
         self.compounding = compounding
@@ -32,10 +37,26 @@ class GermanPreprocessor(object):
         result = []
 
         for word in sentence:
+            if self.pronouns:
+                ein_match = ein_type_pronouns.match(word)
+                der_match = der_type_pronouns.match(word)
+
             if self.contracting and word in contractions_set:
                 result.extend(uncontract[word])
+            elif self.pronouns and ein_match:
+                result.append(ein_match[1])
+                result.append("<<"+ein_match[2])
+            elif self.pronouns and der_match:
+                result.append(der_match[1])
+                result.append("<<"+der_match[2])
+            elif self.compounding and word.find(">><<") > -1:
+                compound_parts = word.split(">><<")
+                result.append(compound_parts[0])
+                for w in compound_parts[:1]:
+                    result.append(">><<")
+                    result.append(w.capitalize())
             else:
-                result.append[word]
+                result.append(word)
 
         return result
 
@@ -49,11 +70,22 @@ class GermanPostprocessor(object):
     def __call__(self, sentence):
         result = []
 
+        compound = False
         for word in sentence:
             if self.contracting and word in contract \
                     and result and result[-1] in contract[word]:
                 result[-1] = contract[word][result[-1]]
+            elif self.pronouns and word.startswith("<<"):
+                if result:
+                    result[-1] += word[2:]
+            elif self.compounding and result and word == ">><<":
+                compound = True
+            elif self.compounding and compound:
+                # TODO inserting 's'
+                result[-1] += word.lower()
             else:
                 result.append(word)
+
+        result[0] = result[0].capitalize()
 
         return result
