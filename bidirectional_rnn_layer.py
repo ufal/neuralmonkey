15b2a61990adddb79cb7f80abf18ea262dbcddae
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2016-04-13 14:30:21 +0200

    refactor: bidirectional rnn layer separation

---
+++ bidirectional_rnn_layer.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+from tensorflow.models.rnn import rnn
+from tensorflow.python.ops import array_ops
+
+class BidirectionalRNNLayer(object):
+    """
+    Bidirectional RNN Layer class - forward and backward RNN layers in one.
+   
+    """
+
+
+    def __init__(self, forward_cell, backward_cell, inputs, sentence_lengths_placeholder):
+        """
+        Creates new BiRNN layer.
+
+        Args:
+        cell - the type of the cell (LSTMCell, GRUCell, NoisyGRUCell, ...)
+        inputs - a list of inputs to the layer
+        sentence_lengths_placeholder - lengths of the sequences in inputs
+
+        """
+        
+
+        with tf.variable_scope('forward'):
+            outputs, last_state = rnn.rnn(
+                cell=forward_cell,
+                inputs=inputs,
+                dtype=tf.float32,
+                sequence_length=sentence_lengths_placeholder)
+
+
+        with tf.variable_scope('backward'):
+            outputs_rev_rev, last_state_rev = rnn.rnn(
+                cell=backward_cell,
+                inputs=self._reverse_seq(inputs, sentence_lengths_placeholder),
+                dtype=tf.float32,
+                sequence_length=sentence_lengths_placeholder)
+
+            outputs_rev=self._reverse_seq(outputs_rev_rev, sentence_lengths_placeholder)
+
+                
+        self.outputs_bidi = [tf.concat(1, [o1, o2]) for o1, o2 in zip(outputs, reversed(outputs_rev))]
+        self.encoded = tf.concat(1, [last_state, last_state_rev])
+
+
+    def _reverse_seq(self, input_seq, lengths):
+        """Reverse a list of Tensors up to specified lengths.
+
+        Args:
+            input_seq: Sequence of seq_len tensors of dimension (batch_size, depth)
+            lengths:   A tensor of dimension batch_size, containing lengths for each
+                       sequence in the batch. If "None" is specified, simply reverses
+                       the list.
+
+        Returns:
+            time-reversed sequence
+        """
+        if lengths is None:
+            return list(reversed(input_seq))
+
+        for input_ in input_seq:
+            input_.set_shape(input_.get_shape().with_rank(2))
+
+        # Join into (time, batch_size, depth)
+        s_joined = array_ops.pack(input_seq)
+
+        # Reverse along dimension 0
+        s_reversed = array_ops.reverse_sequence(s_joined, lengths, 0, 1)
+        # Split again into list
+        result = array_ops.unpack(s_reversed)
+        return result
+
--- sentence_encoder.py
+++ sentence_encoder.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-from tensorflow.python.ops import array_ops
-from tensorflow.models.rnn import rnn_cell, rnn, seq2seq
-
+from bidirectional_rnn_layer import BidirectionalRNNLayer
+from tensorflow.models.rnn import rnn_cell
 from noisy_gru_cell import NoisyGRUCell
+
 
 class SentenceEncoder(object):
     def __init__(self, max_input_len, vocabulary, embedding_size, rnn_size, dropout_placeholder,
@@ -18,59 +18,23 @@ class SentenceEncoder(object):
 
             dropped_embedded_inputs = [tf.nn.dropout(i, dropout_placeholder) for i in embedded_inputs]
 
-            with tf.variable_scope('forward'):
-                if use_noisy_activations:
-                    gru = rnn_cell.GRUCell(rnn_size, input_size=embedding_size)
-                else:
-                    gru = NoisyGRUCell(rnn_size, is_training, input_size=embedding_size)
-                outputs, last_state = rnn.rnn(
-                    cell=gru,
-                    inputs=dropped_embedded_inputs, dtype=tf.float32,
-                    sequence_length=self.sentence_lengths)
+            if use_noisy_activations:
+                forward_gru = NoisyGRUCell(rnn_size, is_training, input_size=embedding_size)
+                backward_gru = NoisyGRUCell(rnn_size, is_training, input_size=embedding_size)
 
-            with tf.variable_scope('backward'):
-                if use_noisy_activations:
-                    gru_rev = rnn_cell.GRUCell(rnn_size, input_size=embedding_size)
-                else:
-                    gru_rev = NoisyGRUCell(rnn_size, is_training, input_size=embedding_size)
-                outputs_rev_rev, last_state_rev = rnn.rnn(
-                    cell=gru_rev,
-                    inputs=self._reverse_seq(dropped_embedded_inputs, self.sentence_lengths), dtype=tf.float32,
-                    sequence_length=self.sentence_lengths)
+            else:
+                forward_gru = rnn_cell.GRUCell(rnn_size, input_size=embedding_size)
+                backward_gru = rnn_cell.GRUCell(rnn_size, input_size=embedding_size)
+                
 
-                outputs_rev=self._reverse_seq(outputs_rev_rev, self.sentence_lengths)
-
-            outputs_bidi = [tf.concat(1, [o1, o2]) for o1, o2 in zip(outputs, reversed(outputs_rev))]
-
-            self.encoded = tf.concat(1, [last_state, last_state_rev])
+            bidi_layer = BidirectionalRNNLayer(forward_gru,
+                                               backward_gru,
+                                               dropped_embedded_inputs,
+                                               self.sentence_lengths)
+            
+            self.outputs_bidi = bidi_layer.outputs_bidi
+            self.encoded = bidi_layer.encoded
 
             self.attention_tensor = \
-                    tf.concat(1, [tf.expand_dims(o, 1) for o in outputs_bidi])
+                    tf.concat(1, [tf.expand_dims(o, 1) for o in self.outputs_bidi])
                     #tf.transpose(tf.concat(1, [tf.expand_dims(o, 1) for o in outputs_bidi]), [0, 2, 1])
-
-    def _reverse_seq(self, input_seq, lengths):
-      """Reverse a list of Tensors up to specified lengths.
-
-      Args:
-        input_seq: Sequence of seq_len tensors of dimension (batch_size, depth)
-        lengths:   A tensor of dimension batch_size, containing lengths for each
-                   sequence in the batch. If "None" is specified, simply reverses
-                   the list.
-
-      Returns:
-        time-reversed sequence
-      """
-      if lengths is None:
-        return list(reversed(input_seq))
-
-      for input_ in input_seq:
-        input_.set_shape(input_.get_shape().with_rank(2))
-
-      # Join into (time, batch_size, depth)
-      s_joined = array_ops.pack(input_seq)
-
-      # Reverse along dimension 0
-      s_reversed = array_ops.reverse_sequence(s_joined, lengths, 0, 1)
-      # Split again into list
-      result = array_ops.unpack(s_reversed)
-      return result
