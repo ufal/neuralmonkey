8018df92d55897f996fde6b241de7c4e01cdd5a4
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-04-13 15:13:04 +0200

    Nan causing implementation of batch normalization

--- noisy_gru_cell.py
+++ noisy_gru_cell.py
@@ -1,45 +1,70 @@
 import tensorflow as tf
-from tensorflow.models.rnn.rnn_cell import RNNCell, linear
+from tensorflow.models.rnn.rnn_cell import RNNCell
 import math
 
 class NoisyGRUCell(RNNCell):
-  """
-  Gated Recurrent Unit cell (cf. http://arxiv.org/abs/1406.1078) with noisy
-  activation functions (http://arxiv.org/abs/1603.00391). The theano code is
-  availble at https://github.com/caglar/noisy_units.
+    """
+    Gated Recurrent Unit cell (cf. http://arxiv.org/abs/1406.1078) with noisy
+    activation functions (http://arxiv.org/abs/1603.00391). The theano code is
+    availble at https://github.com/caglar/noisy_units.
 
-  It is based on the TensorFlow implementatin of GRU just the activation
-  function are changed for the noisy ones.
-  """
-  def __init__(self, num_units, training, input_size=None):
-    self._num_units = num_units
-    self._input_size = num_units if input_size is None else input_size
-    self.training = training
+    It is based on the TensorFlow implementatin of GRU just the activation
+    function are changed for the noisy ones.
+    """
+    def __init__(self, num_units, training, input_size=None, batch_normalize=False):
+        """
+        Instantiates the recurrent unit.
 
-  @property
-  def input_size(self):
-    return self._input_size
+        Args:
 
-  @property
-  def output_size(self):
-    return self._num_units
+            num_units: Number of hidden units.
 
-  @property
-  def state_size(self):
-    return self._num_units
+            training: Placeholder telling whether we are in the training or
+                inference time.
 
-  def __call__(self, inputs, state, scope=None):
-    """Gated recurrent unit (GRU) with nunits cells."""
-    with tf.variable_scope(scope or type(self).__name__):  # "GRUCell"
-      with tf.variable_scope("Gates"):  # Reset gate and update gate.
-        # We start with bias of 1.0 to not reset and not update.
-        r, u = tf.split(1, 2, linear([inputs, state],
-                                            2 * self._num_units, True, 1.0))
-        r, u = noisy_sigmoid(r, self.training), noisy_sigmoid(u, self.training)
-      with tf.variable_scope("Candidate"):
-        c = noisy_tanh(linear([inputs, r * state], self._num_units, True), self.training)
-      new_h = u * state + (1 - u) * c
-    return new_h, new_h
+            input_size: Size of the input (if it is different from the nuber of
+                huidden units).
+
+            batch_norm: Flag whther the batch normalizatigon should be used.
+
+        """
+
+
+        self._num_units = num_units
+        self._input_size = num_units if input_size is None else input_size
+        self.training = training
+        self.batch_norm = batch_normalize
+
+    @property
+    def input_size(self):
+        return self._input_size
+
+    @property
+    def output_size(self):
+        return self._num_units
+
+    @property
+    def state_size(self):
+        return self._num_units
+
+    def __call__(self, inputs, state, scope=None):
+        """Gated recurrent unit (GRU) with nunits cells."""
+        with tf.variable_scope(scope or type(self).__name__):    # "GRUCell"
+            with tf.variable_scope("Gates"):    # Reset gate and update gate.
+                # We start with bias of 1.0 to not reset and not update.
+
+                r = linear([inputs, state], self._num_units, True, 1.0, "remember", batch_normalize=self.batch_norm, is_training=self.training)
+                u = linear([inputs, state], self._num_units, True, 1.0, "update", batch_normalize=self.batch_norm, is_training=self.training)
+
+                r, u = noisy_sigmoid(r, self.training), noisy_sigmoid(u, self.training)
+
+            with tf.variable_scope("Candidate"):
+                c = noisy_tanh(linear([inputs, r * state], self._num_units, True), self.training)
+                if self.batch_norm:
+                    c = batch_norm(c, self._num_units, self.training)
+            new_h = u * state + (1 - u) * c
+
+        return new_h, new_h
 
 
 def noisy_activation(x, generic, linearized, training, alpha=1.1, c=0.5):
@@ -89,3 +114,63 @@ def noisy_sigmoid(x, training):
 
 def noisy_tanh(x, training):
     return noisy_activation(x, hard_tanh, lambda y: y, training)
+
+
+def batch_norm(x, n_out, phase_train, scope='batch_norm', affine=True):
+    """
+    Batch normalization for fully connected layers.
+    Taken from http://stackoverflow.com/questions/33949786/how-could-i-use-batch-normalization-in-tensorflow
+    and modified to work with the fully connected layers.
+
+    Arguments:
+        x:           Tensor, 4D BHWD input maps
+        n_out:       integer, depth of input maps
+        phase_train: boolean tf.Variable, true indicates training phase
+        scope:       string, variable scope
+        affine:      whether to affine-transform outputs
+
+    Return:
+        normed:      batch-normalized maps
+
+    """
+
+    with tf.variable_scope(scope):
+        reshaped_x = tf.reshape(x, [-1, 1, 1, n_out])
+        beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
+                           name='beta', trainable=True)
+        gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
+                            name='gamma', trainable=affine)
+
+        batch_mean, batch_var = tf.nn.moments(reshaped_x, [0,1,2], name='moments')
+        ema = tf.train.ExponentialMovingAverage(decay=0.9)
+        ema_apply_op = ema.apply([batch_mean, batch_var])
+        ema_mean, ema_var = ema.average(batch_mean), ema.average(batch_var)
+        def mean_var_with_update():
+            with tf.control_dependencies([ema_apply_op]):
+                return tf.identity(batch_mean), tf.identity(batch_var)
+        mean, var = tf.cond(phase_train,
+            mean_var_with_update,
+            lambda: (ema_mean, ema_var))
+
+        normed = tf.nn.batch_norm_with_global_normalization(reshaped_x, mean, var,
+            beta, gamma, 1e-3, affine)
+        return tf.reshape(normed, shape=[-1, n_out])
+
+
+def linear(args, output_size, bias, bias_start=0.0, scope=None, batch_normalize=False, is_training=None):
+    with tf.variable_scope("linear" if scope is None else scope):
+        projected = []
+        for i, arg in enumerate(args):
+            input_size = arg.get_shape()[1].value
+            W_i = tf.get_variable(shape=[input_size, output_size], name="proj_{}".format(i))
+            proj = tf.matmul(arg, W_i)
+            if batch_normalize:
+                proj = batch_norm(proj, output_size, is_training)
+            projected.append(proj)
+
+        res = sum(projected)
+
+        bias_term = tf.get_variable(
+                "Bias", [output_size],
+                initializer=tf.constant_initializer(bias_start))
+        return res + bias_term
