6364e731c6a9550e95d0b86e95edf4474592506d
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-04-22 00:28:46 +0200

    Encoder feed dict

--- sentence_encoder.py
+++ sentence_encoder.py
@@ -7,6 +7,9 @@ from noisy_gru_cell import NoisyGRUCell
 class SentenceEncoder(object):
     def __init__(self, max_input_len, vocabulary, embedding_size, rnn_size, dropout_placeholder,
                  is_training, use_noisy_activations=False, name="sentence_encoder"):
+        self.vocabulary = vocabulary
+        self.max_input_len = max_input_len
+
         with tf.variable_scope(name):
             self.inputs = \
                     [tf.placeholder(tf.int32, shape=[None], name="input_{}".format(i)) for i in range(max_input_len + 2)]
@@ -38,3 +41,16 @@ class SentenceEncoder(object):
             self.attention_tensor = \
                     tf.concat(1, [tf.expand_dims(o, 1) for o in self.outputs_bidi])
                     #tf.transpose(tf.concat(1, [tf.expand_dims(o, 1) for o in outputs_bidi]), [0, 2, 1])
+
+    def feed_dict(self, sentences, batch_size, dicts=None):
+        if dicts == None:
+            dicts = [{} for _ in range(len(sentences) / batch_size + int(len(sentences) % batch_size))]
+
+        for fd, start in zip(dicts, range(0, len(sentences, batch_size))):
+            fd[self.sentence_lengths] = np.array([min(args.maximum_output, len(s)) + 2 for s in src_sentences])
+            vectors, _ = \
+                    self.vocabulary.sentences_to_tensor(sentences, self.max_input_len, train=train)
+            for words_plc, words_tensor in zip(self.inputs, vectors):
+                fd[words_plc] = words_tensor
+
+        return dicts
