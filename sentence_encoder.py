f88faa86265d1e44e4cec4df88068e3ffc4e5adf
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-05-04 10:53:31 +0200

    add identical sentence encoder from parent

--- sentence_encoder.py
+++ sentence_encoder.py
@@ -6,7 +6,8 @@ import numpy as np
 
 class SentenceEncoder(object):
     def __init__(self, max_input_len, vocabulary, embedding_size, rnn_size, dropout_placeholder,
-                 is_training, use_noisy_activations=False, attention_type=None, attention_fertility=None, name="sentence_encoder"):
+                 is_training, use_noisy_activations=False, attention_type=None,
+                 attention_fertility=None, name="sentence_encoder", parent_encoder=None):
         self.vocabulary = vocabulary
         self.max_input_len = max_input_len
 
@@ -15,23 +16,27 @@ class SentenceEncoder(object):
                     [tf.placeholder(tf.int32, shape=[None], name="input_{}".format(i)) for i in range(max_input_len + 2)]
             self.sentence_lengths = tf.placeholder(tf.int64, shape=[None], name="sequence_lengths")
 
-            self.word_embeddings = tf.Variable(tf.random_uniform([len(vocabulary), embedding_size], -1.0, 1.0))
+            if parent_encoder:
+                self.word_embeddings = parent_encoder.word_embeddings
+            else:
+                self.word_embeddings = tf.Variable(tf.random_uniform([len(vocabulary), embedding_size], -1.0, 1.0))
 
             embedded_inputs = [tf.nn.embedding_lookup(self.word_embeddings, input_) for input_ in self.inputs]
-
             dropped_embedded_inputs = [tf.nn.dropout(i, dropout_placeholder) for i in embedded_inputs]
 
-            if use_noisy_activations:
-                forward_gru = NoisyGRUCell(rnn_size, is_training, input_size=embedding_size)
-                backward_gru = NoisyGRUCell(rnn_size, is_training, input_size=embedding_size)
-
+            if parent_encoder:
+                self.forward_gru = parent_encoder.forward_gru
+                self.backward_gru = parent_encoder.backward_gru
             else:
-                forward_gru = rnn_cell.GRUCell(rnn_size, input_size=embedding_size)
-                backward_gru = rnn_cell.GRUCell(rnn_size, input_size=embedding_size)
+                if use_noisy_activations:
+                    self.forward_gru = NoisyGRUCell(rnn_size, is_training, input_size=embedding_size)
+                    self.backward_gru = NoisyGRUCell(rnn_size, is_training, input_size=embedding_size)
+                else:
+                    self.forward_gru = rnn_cell.GRUCell(rnn_size, input_size=embedding_size)
+                    self.backward_gru = rnn_cell.GRUCell(rnn_size, input_size=embedding_size)
 
-
-            bidi_layer = BidirectionalRNNLayer(forward_gru,
-                                               backward_gru,
+            bidi_layer = BidirectionalRNNLayer(self.forward_gru,
+                                               self.backward_gru,
                                                dropped_embedded_inputs,
                                                self.sentence_lengths)
 
