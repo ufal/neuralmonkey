0a79d439a58018938f5dd413e7f7478942070d89
Tomáš Musil <tomik.musil@gmail.com>
2016-05-21 12:24:36 +0200

    types for Vocabulary

--- vocabulary.py
+++ vocabulary.py
@@ -2,8 +2,14 @@ import random
 import numpy as np
 import regex as re
 
+try:
+    #pylint: disable=unused-import,bare-except
+    from typing import List, Tuple
+except:
+    pass
+
 class Vocabulary(object):
-    def __init__(self, tokenized_text=None):
+    def __init__(self, tokenized_text=None): # type: List[str] -> Vocabulary
         self.word_to_index = {}
         self.index_to_word = []
         self.word_count = {}
@@ -13,18 +19,18 @@ class Vocabulary(object):
         if tokenized_text:
             self.add_tokenized_text(tokenized_text)
 
-    def add_word(self, word):
+    def add_word(self, word): # type: str -> None
         if word not in self.word_to_index:
             self.word_to_index[word] = len(self.index_to_word)
             self.index_to_word.append(word)
             self.word_count[word] = 0
         self.word_count[word] += 1
 
-    def add_tokenized_text(self, tokenized_text):
+    def add_tokenized_text(self, tokenized_text): # type: List[str] -> None
         for word in tokenized_text:
             self.add_word(word)
 
-    def get_train_word_index(self, word):
+    def get_train_word_index(self, word): # type: str -> int
         if word not in self.word_count or self.word_count[word] <= 1:
             if random.random > 0.5:
                 return self.word_to_index["<unk>"]
@@ -33,19 +39,19 @@ class Vocabulary(object):
         else:
             return self.word_to_index[word]
 
-    def get_word_index(self, word):
+    def get_word_index(self, word): # type: str -> int
         if word not in self.word_count:
             return self.word_to_index["<unk>"]
         else:
             return self.word_to_index[word]
 
-    def __len__(self):
+    def __len__(self): # type: int
         return len(self.index_to_word)
 
-    def __contains__(self, word):
+    def __contains__(self, word): # type: bool
         return word in self.word_to_index
 
-    def trunkate(self, size):
+    def trunkate(self, size): # type: int -> None
         """
         Trunkates the Vocabulary to requested size by keep only the most
         frequent tokens.
@@ -72,6 +78,7 @@ class Vocabulary(object):
             self.word_to_index[word] = index
 
     def sentences_to_tensor(self, sentences, max_len, train=False):
+        # type: (List[List[str]], int, bool) -> Tuple[np.Array, np.Array]
         """
         Generates the tensor representation for the provided sentences.
 
@@ -102,6 +109,8 @@ class Vocabulary(object):
         return word_indices, weights
 
     def vectors_to_sentences(self, vectors):
+        #pylint: disable=fixme
+        # TODO type
         sentences = [[] for _ in range(vectors[0].shape[0])]
 
         for vec in vectors:
