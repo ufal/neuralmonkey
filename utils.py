490ae108d8b25eb1609ea4196b5536b605ecd6fc
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-05-31 13:44:02 +0200

    document utils

--- utils.py
+++ utils.py
@@ -5,11 +5,13 @@ from termcolor import colored
 log_file = None
 
 def set_log_file(path):
+    """ Sets up the file where the logging will be done. """
     global log_file
     log_file = codecs.open(path, 'w', 'utf-8', buffering=0)
 
 
 def log_print(text):
+    """ Prints a string both to console and a log file is it si defined. """
     if log_file:
         log_file.write(text+"\n")
         log_file.flush()
@@ -17,6 +19,7 @@ def log_print(text):
 
 
 def log(message, color='yellow'):
+    """ Logs message with a colored timestamp. """
     log_print("{}: {}".format(colored(time.strftime("%Y-%m-%d %H:%M:%S"), color), message))
 
 
