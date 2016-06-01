2de23afc1f9a066a849d05883dad6d2a4f647767
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-06-01 09:50:19 +0200

    list and style experiments

--- logbook/logbook.js
+++ logbook/logbook.js
@@ -4,8 +4,16 @@ function loadExperiments() {
     $.get("/experiments", function(data) {
         experiments = data.experiments
         $("#experiments").empty()
-        for(ex in experiments) {
-            $("#experiments").append("<div>"+ex+"</div>")
+        for(var i in experiments) {
+            var experiment_box = $("<div></div>")
+                .text(experiments[i])
+                .addClass("exp_item")
+                .click(function () {
+                    $(".exp_item_selected").removeClass("exp_item_selected");
+                    $(this).addClass("exp_item_selected");
+                    selectedExperiment = $(this).text()
+                });
+            $("#experiments").append(experiment_box)
         }
     });
 }
