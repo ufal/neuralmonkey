e355896a20b471db29520c824c2918f8bcd382dc
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-06-06 11:39:14 +0200

    perplexity for training LM and an example

---
+++ perplexity_runner.py
@@ -0,0 +1,59 @@
+"""
+
+This module contains an implementation of a runner that is supposed to be used
+in case we train a language model. Instead of decoding sentences in computes
+its perplexities given the decoder.
+
+"""
+
+import numpy as np
+from tensorflow.models.rnn.seq2seq import sequence_loss_by_example
+
+from learning_utils import feed_dicts
+
+class PerplexityRunner(object):
+    def __init__(self, decoder, batch_size):
+        self.decoder = decoder
+        self.batch_size = batch_size
+        self.vocabulary = decoder.vocabulary
+
+        self.cross_entropies_op = \
+                sequence_loss_by_example(decoder.gt_logits,
+                                         decoder.targets,
+                                         decoder.weights_ins,
+                                         len(decoder.vocabulary))
+
+    def __call__(self, sess, dataset, coders):
+        if not dataset.has_series(self.decoder.data_id):
+            raise Exception("Dataset must have the target values ({}) for computing perplexity.".\
+                    format(self.decoder.data_id))
+
+        batched_dataset = dataset.batch_dataset(self.batch_size)
+        losses = [self.decoder.loss_with_gt_ins,
+                  self.decoder.loss_with_decoded_ins]
+        perplexities = []
+
+        loss_with_gt_ins = 0.0
+        loss_with_decoded_ins = 0.0
+        batch_count = 0
+        for batch in batched_dataset:
+            batch_count += 1
+            batch_feed_dict = feed_dicts(batch, coders, train=False)
+            cross_entropies, opt_loss, dec_loss = \
+                sess.run([self.cross_entropies_op] + losses, feed_dict=batch_feed_dict)
+            perplexities.extend([2 ** xent for xent in cross_entropies])
+            loss_with_gt_ins += opt_loss
+            loss_with_decoded_ins += dec_loss
+
+        return perplexities, \
+               loss_with_gt_ins / batch_count, \
+               loss_with_decoded_ins / batch_count
+
+def perplexity(perplexities, _):
+    """
+    Gets the average perplexity of a sentence. The list of perplexities is
+    provided instead of the list of decoded sentences, the reference sentences
+    are not needed at all.
+    """
+    return np.mean(perplexities)
+
