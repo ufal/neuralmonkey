5318c4bc64e50228329e0e4c2201e8784b256609
Tomáš Musil <tomik.musil@gmail.com>
2016-06-21 15:17:21 +0200

    check more types

--- mypy_run.sh
+++ mypy_run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-mypy --py2 vocabulary.py learning_utils.py -s
+mypy --py2 -s \
+       dataset.py \
+       vocabulary.py \
+       learning_utils.py \
+       config_loader.py
+
+if (( $? ))
+then
+	exit $?
+else
+	echo "Typecheck OK."
+fi
