3b164f17dd9952c4e8dfc88ecf370efb01235711
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-06-28 16:23:09 +0200

    tf9 to prepare_env

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ ! -d tf ]; then
-    virtualenv --system-site-packages tf
+    virtualenv -p python3 --system-site-packages tf
 fi
 
 if [ ! -d tf-gpu ]; then
-    virtualenv --system-site-packages tf-gpu
+    virtualenv -p python3 --system-site-packages tf-gpu
 fi
 cat tf-gpu/bin/activate - << EOF > tf-gpu/bin/activate-cuda
 CUDA_DIR=/usr/local/cuda
@@ -18,19 +18,21 @@ if [ -d \$CUDA_DIR ] ; then
 fi
 EOF
 
+TF_VERSION=tensorflow-0.8.0-cp27-none-linux_x86_64.whl
+
 source tf/bin/activate
-wget https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp27-none-linux_x86_64.whl
-pip install tensorflow-0.8.0-cp27-none-linux_x86_64.whl
+wget https://storage.googleapis.com/tensorflow/linux/cpu/$TF_VERSION
+pip install $TF_VERSION
 deactivate
 
-rm tensorflow-0.8.0-cp27-none-linux_x86_64.whl
+rm $TF_VERSION
 
 source tf-gpu/bin/activate
-wget https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.8.0-cp27-none-linux_x86_64.whl
-pip install tensorflow-0.8.0-cp27-none-linux_x86_64.whl
+wget https://storage.googleapis.com/tensorflow/linux/gpu/$TF_VERSION
+pip install $TF_VERSION
 deactivate
 
-rm tensorflow-0.8.0-cp27-none-linux_x86_64.whl
+rm $TF_VERSION
 
 for ENV in tf tf-gpu; do
     source $ENV/bin/activate
@@ -44,13 +46,3 @@ for ENV in tf tf-gpu; do
 
     deactivate
 done
-
-# Decompounder
-#cd tf
-#if [ ! -d jwordsplitter ]; then
-#    git clone https://github.com/danielnaber/jwordsplitter
-#    cd jwordsplitter
-#    ./build.sh
-#fi
-#cd ../..
-#ln -s tf/jwordsplitter tf-gpu/jwordsplitter
