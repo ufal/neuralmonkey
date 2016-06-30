b9f2cdc69af8be6d4133b13a4338571eba22fde5
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2016-06-30 17:18:17 +0200

    BLEU implementation

--- neuralmonkey/evaluators/bleu.py
+++ neuralmonkey/evaluators/bleu.py
@@ -1,11 +1,10 @@
-from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
+from __future__ import division
+from collections import Counter
+import numpy as np
 
 class BLEUEvaluator(object):
-    bleu_smoothing = SmoothingFunction(epsilon=0.01).method1
-
     def __init__(self, n=4, deduplicate=False, name=None):
         self.n = n
-        self.weights = [1.0/n for _ in range(n)]
         self.deduplicate = deduplicate
 
         if name is not None:
@@ -16,7 +15,6 @@ class BLEUEvaluator(object):
                 self.name += "-dedup"
 
 
-
     def __call__(self, decoded, references):
         # type: (List[List[str]], List[List[str]]) -> float
         listed_references = [[s] for s in references]
@@ -24,8 +22,177 @@ class BLEUEvaluator(object):
         if self.deduplicate:
             decoded = BLEUEvaluator._deduplicate_sentences(decoded)
 
-        return 100 * corpus_bleu(listed_references, decoded, self.weights,
-                                 BLEUEvaluator.bleu_smoothing)
+        return 100 * BLEUEvaluator.bleu(decoded, listed_references, self.n)
+
+
+    @staticmethod
+    def ngram_counts(sentence, n, lowercase, delimiter=" "):
+        # type: (List[str], int, str) -> Counter
+        """Get n-grams from a sentence
+
+        Arguments:
+            sentence: Sentence as a list of words
+            n: n-gram order
+            lowercase: Convert ngrams to lowercase
+            delimiter: delimiter to use to create counter entries
+        """
+        counts = Counter()
+
+        for begin in range(len(sentence) - n + 1):
+            ngram = delimiter.join(sentence[begin:begin + n])
+            if lowercase:
+                ngram = ngram.lower()
+
+            counts[ngram] += 1
+
+        return counts
+
+
+    @staticmethod
+    def merge_max_counters(counters):
+        # type: (List[Counter]) -> Counter
+        """Merge counters using maximum values"""
+        merged = Counter()
+
+        for counter in counters:
+            for key in counter:
+                merged[key] = max(merged[key], counter[key])
+
+        return merged
+
+
+    @staticmethod
+    def modified_ngram_precision(hypotheses, references_list, n,
+                                 case_sensitive):
+        # type: (List[List[str]], List[List[List[str]]], int, bool) -> float
+        """Computes the modified n-gram precision on a list of sentences
+
+        Arguments:
+            hypothesis: List of output sentences as lists of words
+            references: List of lists of reference sentences (as lists of words)
+            n: n-gram order
+            case_sensitive: Whether to perform case-sensitive computation
+        """
+        corpus_true_positives = 0
+        corpus_generated_length = 0
+
+        for hypothesis, references in zip(hypotheses, references_list):
+            reference_counters = []
+
+            for reference in references:
+                counter = BLEUEvaluator.ngram_counts(reference, n,
+                                                     not case_sensitive)
+                reference_counters.append(counter)
+
+            reference_counts = BLEUEvaluator.merge_max_counters(
+                reference_counters)
+            hypothesis_counts = BLEUEvaluator.ngram_counts(hypothesis, n,
+                                                           not case_sensitive)
+
+            true_positives = 0
+            for ngram in hypothesis_counts:
+                true_positives += reference_counts[ngram]
+
+            corpus_true_positives += true_positives
+            corpus_generated_length += sum(hypothesis_counts.values())
+
+        if corpus_generated_length == 0:
+            return 0
+
+        return corpus_true_positives / corpus_generated_length
+
+
+    @staticmethod
+    def effective_reference_length(hypotheses, references_list):
+        # type: (List[List[str]], List[List[List[str]]]) -> int
+        """Computes the effective reference corpus length (based on best match
+        length)
+
+        Arguments:
+            hypotheses: List of output sentences as lists of words
+            references: List of lists of references (as lists of words)
+        """
+
+        eff_ref_length = 0
+
+        for hypothesis, references in zip(hypotheses, references_list):
+            hypothesis_length = len(hypothesis)
+
+            best_diff = np.inf
+            best_match_length = 0
+
+            for reference in references:
+                diff = np.abs(len(reference) - hypothesis_length)
+
+                if diff < best_diff:
+                    best_diff = diff
+                    best_match_length = len(reference)
+
+            eff_ref_length += best_match_length
+
+        return eff_ref_length
+
+
+    # to mainain same API with the function above
+    # pylint: disable=unused-argument
+    @staticmethod
+    def minimum_reference_length(hypotheses, references_list):
+        # type: (List[List[str]], List[List[List[str]]]) -> int
+        """Computes the effective reference corpus length (based on the shortest
+        reference sentence length)
+
+        Arguments:
+            hypotheses: List of output sentences as lists of words
+            references: List of lists of references (as lists of words)
+        """
+
+        eff_ref_length = 0
+
+        for references in references_list:
+            shortest_length = np.inf
+
+            for reference in references:
+                if len(reference) > shortest_length:
+                    shortest_length = len(reference)
+
+            eff_ref_length = shortest_length
+
+        return eff_ref_length
+
+
+    @staticmethod
+    def bleu(hypotheses, references, ngrams=4, case_sensitive=True):
+        # Type: (List[List[str]], List[List[List[str]]]) -> float
+        # pylint: disable=invalid-name
+        """Computes BLEU on a corpus with multiple references using uniform
+        weights.
+
+        Arguments:
+            hypotheses: List of hypotheses
+            references: List of references. There can be more than one reference
+            ngram: Maximum order of n-grams. Default 4.
+            case_sensitive: Perform case-sensitive computation. Default True.
+        """
+        log_bleu = 0
+        weight = 1 / ngrams
+
+        for order in range(1, ngrams + 1):
+            prec = BLEUEvaluator.modified_ngram_precision(
+                hypotheses, references, order, case_sensitive)
+
+            if prec == 0.0:
+                return 0
+
+            log_bleu += weight * np.log(prec)
+
+        # add brevity penalty
+        r = BLEUEvaluator.minimum_reference_length(hypotheses, references)
+        c = sum([len(hyp) for hyp in hypotheses])
+
+        bp = min(1 - r/c, 0)
+        log_bleu += bp
+
+        return np.exp(log_bleu)
 
 
     @staticmethod
