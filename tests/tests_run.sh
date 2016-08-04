fe9ffcc12aa123fedc343b44fd2c537388072d42
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2016-08-04 15:27:56 +0200

    cleanup + new executable

--- tests/tests_run.sh
+++ tests/tests_run.sh
@@ -6,12 +6,12 @@ if [ ! -f tests/data/train.tc.en ]; then
 	wget -P tests/data http://ufallab.ms.mff.cuni.cz/~musil/{train,val}.tc.{en,de}
 fi
 
-bin/neuralmonkey-train tests/small.ini
-bin/neuralmonkey-run tests/small.ini tests/test_data.ini
+bin/nmonkey train tests/small.ini
+bin/nmonkey run tests/small.ini tests/test_data.ini
 
-bin/neuralmonkey-server --configuration=tests/small.ini --port=5000 &
-SERVER_PID=$!
-sleep 20
+#bin/neuralmonkey-server --configuration=tests/small.ini --port=5000 &
+#SERVER_PID=$!
+#sleep 20
 
-curl 127.0.0.1:5000 -H "Content-Type: application/json" -X POST -d '{"source": ["I am the eggman.", "I am the walrus ."]}'
-kill $SERVER_PID
+#curl 127.0.0.1:5000 -H "Content-Type: application/json" -X POST -d '{"source": ["I am the eggman.", "I am the walrus ."]}'
+#kill $SERVER_PID
