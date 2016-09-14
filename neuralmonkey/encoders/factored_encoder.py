2c6e92df92460f2fe83a114529e5a2a224d14741
Rihards Krišlauks <rihards.krislauks@gmail.com>
2016-09-15 00:12:45 +0300

    Modify the copy of SentenceEncoder to accept factored inputs

--- neuralmonkey/encoders/factored_encoder.py
+++ neuralmonkey/encoders/factored_encoder.py
@@ -10,19 +10,19 @@ from neuralmonkey.vocabulary import Vocabulary
 
 # tests: mypy
 
-class SentenceEncoder(object):
-    def __init__(self, max_input_len, vocabulary, data_id, embedding_size,
+class FactoredEncoder(object):
+    def __init__(self, max_input_len, vocabularies, data_ids, embedding_sizes,
                  rnn_size, dropout_keep_p=0.5, use_noisy_activations=False,
                  use_pervasive_dropout=False, attention_type=None,
-                 attention_fertility=3, name="sentence_encoder",
-                 parent_encoder=None):
+                 attention_fertility=3, name="sentence_encoder"):
 
         self.name = name
         self.max_input_len = max_input_len
-        assert_type(self, 'vocabulary', vocabulary, Vocabulary)
-        self.vocabulary = vocabulary
-        self.data_id = data_id
-        self.embedding_size = embedding_size
+        for vocabulary in vocabularies:
+            assert_type(self, 'vocabulary', vocabulary, Vocabulary)
+        self.vocabularies = vocabularies
+        self.data_ids = data_ids
+        self.embedding_sizes = embedding_sizes
         self.rnn_size = rnn_size
         self.max_input_len = max_input_len
         self.dropout_keep_p = dropout_keep_p
@@ -31,9 +31,9 @@ class SentenceEncoder(object):
         self.attention_type = attention_type
         self.attention_fertility = attention_fertility
 
-        assert_type(self, 'parent_encoder', parent_encoder, SentenceEncoder,
-                    can_be_none=True)
-        self.parent_encoder = parent_encoder
+        # TODO: checking.py expects a self.data_id field so we add one
+        # for compatability but ideally checking.py should be refactored.
+        self.data_id = self.data_ids[0]
 
         log("Initializing sentence encoder, name: \"{}\"".format(name))
         with tf.variable_scope(name):
@@ -41,41 +41,43 @@ class SentenceEncoder(object):
                                                       name="dropout")
             self.is_training = tf.placeholder(tf.bool, name="is_training")
 
-            self.inputs = [tf.placeholder(tf.int32, shape=[None],
-                                          name="input_{}".format(i))
-                           for i in range(max_input_len + 2)]
+            self.factor_inputs = {data_id: [tf.placeholder(tf.int32, shape=[None],
+                                                           name="{}_input_{}".format(data_id, i))
+                                            for i in range(max_input_len + 2)]
+                                  for data_id in data_ids}
 
-            self.weight_ins = [tf.placeholder(tf.float32, shape=[None],
-                                              name="input_{}".format(i))
-                               for i in range(max_input_len + 2)]
+            self.factor_weight_ins =  {data_id: [tf.placeholder(tf.float32, shape=[None],
+                                                                name="{}_input_{}".format(data_id, i))
+                                                for i in range(max_input_len + 2)]
+                                       for data_id in self.data_ids}
 
             self.weight_tensor = tf.concat(1, [tf.expand_dims(w, 1)
-                                               for w in self.weight_ins])
+                                               for w in self.factor_weight_ins[self.data_ids[0]]])
 
-            self.sentence_lengths = tf.to_int64(sum(self.weight_ins))
+            self.sentence_lengths = tf.to_int64(sum(self.factor_weight_ins[self.data_ids[0]]))
 
-            if parent_encoder:
-                self.word_embeddings = parent_encoder.word_embeddings
+            self.word_embeddings = {data_id: tf.Variable(tf.random_uniform(
+                                                [len(vocabulary), embedding_size], -1.0, 1.0))
+                                    for data_id, vocabulary, embedding_size
+                                        in zip(self.data_ids, self.vocabularies, self.embedding_sizes)}
+
+            embedded_factors = {data_id: [tf.nn.embedding_lookup(self.word_embeddings[data_id], i)
+                                          for i in factor]
+                               for data_id, factor in self.factor_inputs.items()}
+            dropped_embedded_factors = {data_id: [
+                                            tf.nn.dropout(i, self.dropout_placeholder)
+                                            for i in embedded_factor]
+                                       for data_id, embedded_factor in embedded_factors.items()}
+            concatenated_factors = [tf.concat(concat_dim=1, values=related_factors)
+                                   for related_factors in zip(*[dropped_embedded_factors[data_id]
+                                                                for data_id in self.data_ids])]
+
+            if use_noisy_activations:
+                self.forward_gru = NoisyGRUCell(rnn_size, self.is_training)
+                self.backward_gru = NoisyGRUCell(rnn_size, self.is_training)
             else:
-                self.word_embeddings = tf.Variable(tf.random_uniform(
-                    [len(vocabulary), embedding_size], -1.0, 1.0))
-
-            embedded_inputs = [tf.nn.embedding_lookup(self.word_embeddings, i)
-                               for i in self.inputs]
-            dropped_embedded_inputs = [
-                tf.nn.dropout(i, self.dropout_placeholder)
-                for i in embedded_inputs]
-
-            if parent_encoder:
-                self.forward_gru = parent_encoder.forward_gru
-                self.backward_gru = parent_encoder.backward_gru
-            else:
-                if use_noisy_activations:
-                    self.forward_gru = NoisyGRUCell(rnn_size, self.is_training)
-                    self.backward_gru = NoisyGRUCell(rnn_size, self.is_training)
-                else:
-                    self.forward_gru = tf.nn.rnn_cell.GRUCell(rnn_size)
-                    self.backward_gru = tf.nn.rnn_cell.GRUCell(rnn_size)
+                self.forward_gru = tf.nn.rnn_cell.GRUCell(rnn_size)
+                self.backward_gru = tf.nn.rnn_cell.GRUCell(rnn_size)
 
             if use_pervasive_dropout:
 
@@ -102,7 +104,7 @@ class SentenceEncoder(object):
 
             bidi_layer = BidirectionalRNNLayer(self.forward_gru,
                                                self.backward_gru,
-                                               dropped_embedded_inputs,
+                                               concatenated_factors,
                                                self.sentence_lengths)
 
             self.outputs_bidi = bidi_layer.outputs_bidi
@@ -121,22 +123,33 @@ class SentenceEncoder(object):
 
 
     def feed_dict(self, dataset, train=False):
-        sentences = dataset.get_series(self.data_id)
+        factors = {data_id: dataset.get_series(data_id) for data_id in self.data_ids}
 
         res = {}
-        res[self.sentence_lengths] = np.array(
-            [min(self.max_input_len, len(s)) + 2 for s in sentences])
+        # we asume that all factors have equal word counts
+        # this is removed as res should only contain placeholders as keys
+        # res[self.sentence_lengths] = np.array(
+        #     [min(self.max_input_len, len(s)) + 2 for s in factors[self.data_ids[0]]])
 
-        vectors, weights = self.vocabulary.sentences_to_tensor(
-            sentences, self.max_input_len, train=train)
+        factor_vectors_and_weights = {data_id: vocabulary.sentences_to_tensor(
+                                        factors[data_id], self.max_input_len, train=train)
+                                      for data_id, vocabulary in zip(self.data_ids, self.vocabularies)}
 
-        for words_plc, words_tensor in zip(self.inputs, vectors):
-            res[words_plc] = words_tensor
+        for data_id in self.data_ids:
+            inputs = self.factor_inputs[data_id]
+            vectors, _ = factor_vectors_and_weights[data_id]
+            for words_plc, words_tensor in zip(inputs, vectors):
+                res[words_plc] = words_tensor
 
-        res[self.weight_ins[0]] = np.ones(len(sentences))
+        for data_id, weight_ins in self.factor_weight_ins.items():
+            sentences = factors[data_id]
+            res[weight_ins[0]] = np.ones(len(sentences)) 
 
-        for weights_plc, weight_vector in zip(self.weight_ins[1:], weights):
-            res[weights_plc] = weight_vector
+        for data_id in self.data_ids:
+            _, weights = factor_vectors_and_weights[data_id]
+            weight_ins = self.factor_weight_ins[data_id]
+            for weights_plc, weight_vector in zip(weight_ins[1:], weights):
+                res[weights_plc] = weight_vector
 
         if train:
             res[self.dropout_placeholder] = self.dropout_keep_p
