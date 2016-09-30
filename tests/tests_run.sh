48c313988781ba6bb30691f7cf1fbe59e64435ee
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2016-09-30 16:39:31 +0200

    fixing tests

--- tests/tests_run.sh
+++ tests/tests_run.sh
@@ -6,10 +6,16 @@ if [ ! -f tests/data/train.tc.en ]; then
 	wget -P tests/data http://ufallab.ms.mff.cuni.cz/~musil/{train,val}.tc.{en,de}
 fi
 
-bin/neuralmonkey run tests/vocab.ini
-
+bin/neuralmonkey train tests/vocab.ini
 bin/neuralmonkey train tests/small.ini
+
+mkdir -p tests/tmp-vars
+mv tests/tmp-test-output/variables.data.cont-1.0 tests/tmp-vars/variables.data.0
+mv tests/tmp-test-output/variables.data.cont-1.1 tests/tmp-vars/variables.data.1
+
+
 bin/neuralmonkey run tests/small.ini tests/test_data.ini
+bin/neuralmonkey run tests/ensemble.ini tests/test_data.ini
 
 #bin/neuralmonkey-server --configuration=tests/small.ini --port=5000 &
 #SERVER_PID=$!
