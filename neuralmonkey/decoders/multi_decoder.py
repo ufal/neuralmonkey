e4600b890cf1c470922db2d6f559fb0da4102903
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2016-10-08 16:32:31 +0200

    polishing multi_decoder

--- neuralmonkey/decoders/multi_decoder.py
+++ neuralmonkey/decoders/multi_decoder.py
@@ -1,35 +1,47 @@
-import math
 import tensorflow as tf
 import numpy as np
 
-from neuralmonkey.vocabulary import START_TOKEN
-from neuralmonkey.decoding_function import attention_decoder
+from neuralmonkey.vocabulary import PAD_TOKEN
 from neuralmonkey.logging import log
 from neuralmonkey.dataset import Dataset
 
 
 class MultiDecoder(object):
-    """A class that manages parts of the computation graph that are
-    used for the decoding.
+    """The MultiDecoder class wraps a several child decoders into
+    one parent encoder. The Neural Monkey architecture requires the model to
+    have only one decoder, so this class can be used when more than one output
+    sequence should be generated (i.e. multi-task learning).
+
+    The multi decoder object composes of one main decoder and an arbitrary
+    number of additional decoders, called 'regularization' decoders.
+
+    The reason for this division is that during validation, we need to report
+    a single score of the model as a whole, and based on this score, the
+    training process decides whether to save the model variables or not.
+
+    So if the task is translation with POS tagging of the source sentence, the
+    main decoder should be the decoder that generates the target sentence,
+    whereas the sequence labeler used for POS tagging should be included in the
+    regularization decoders list.
+
+    During training, the multi decoder works in the following way: According to
+    the value of the ``_input_selector`` placeholder, the loss corresponds to
+    one of the child decoders (so in multi-task learning, the weights in each
+    batch are updated with respect only to one sub-task). It is therefore a good
+    practice to alternate between batches of different task. This is because we
+    often do not have the training data that cover all tasks in one corpus.
+
     """
 
-    # pylint: disable=too-many-instance-attributes,too-many-locals
-    # Big decoder cannot be simpler. Not sure if refactoring
-    # it into smaller units would be helpful
-    # Some locals may be turned to attributes
-
-    def __init__(self, main_decoder, regularization_decoders, **kwargs):
+    def __init__(self, main_decoder, regularization_decoders):
         """Create a new instance of the multi-decoder.
 
         Arguments:
             main_decoder: The decoder that corresponds to the output which
                 we want at runtime.
 
-            additional_decoders: A list of the decoders among which the multidecoder
-                will switch.
-
-        Keyword arguments:
-
+            regularization_decoders: A list of the decoders among which the
+                multidecoder will switch.
         """
         self.main_decoder = main_decoder
 
@@ -89,36 +101,38 @@ class MultiDecoder(object):
 
 
     def feed_dict(self, dataset, train=False):
-        """TODO: rewrite for multidecoder
-
-        Populate the feed dictionary for the decoder object
+        """Populate the feed dictionary for the decoder object
 
         Decoder placeholders:
-            ``input_selector``
+            ``input_selector``: the index of the child decoder used for
+                                computing the loss
         """
-
         # Two options:
         #  - call feed_dict only on the currently selected child decoder
         #  - call feed_dict preventatively everywhere (with dummy data)
 
         # First option:
-        # fd = self.decoders[self._scheduled_decoder].feed_dict(dataset, train=train)
+        # fd = self.decoders[self._scheduled_decoder].feed_dict(
+        #     dataset, train=train)
+        # First option does not seem to work, so the second option
+        # will have to do.
 
         # Second option:
         # (This is a fallback plan in canse TensorFlow requires us to fill in
         #  the data placeholders for the nodes that are hidden behind a zero
         #  through self.input_selector.)
-        #
+
+        # pylint: disable=invalid-name
+        # fd stands for feed_dict
         fd = {}
-        for i, d in enumerate(self._training_decoders):
+        for i, decoder in enumerate(self._training_decoders):
             if i == self._scheduled_decoder:
-                fd_i = d.feed_dict(dataset, train=train)
+                fd_i = decoder.feed_dict(dataset, train=train)
             else:
-                # serie je generator seznamu slov (vet)
-                # <pad> is PAD_TOKEN
-                serie = [["<pad>"] for _ in range(len(dataset))]
-                dummy_dataset = Dataset("dummy", {d.data_id: serie}, {})
-                fd_i = d.feed_dict(dummy_dataset, train=train)
+                # serie is a generator of lists of words (i.e. sentences)
+                serie = [[PAD_TOKEN] for _ in range(len(dataset))]
+                dummy_dataset = Dataset("dummy", {decoder.data_id: serie}, {})
+                fd_i = decoder.feed_dict(dummy_dataset, train=train)
             fd.update(fd_i)
 
         # We now need to set the value of our input_selector placeholder
