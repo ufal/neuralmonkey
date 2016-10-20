622686952b4ba569a9fd3411ddfe625b28a8e10d
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2016-10-20 16:52:28 +0200

    [ci skip] first dynamization of sentence encoder

--- neuralmonkey/encoders/sentence_encoder.py
+++ neuralmonkey/encoders/sentence_encoder.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import numpy as np
 
 from neuralmonkey.logging import log
-from neuralmonkey.nn.bidirectional_rnn_layer import BidirectionalRNNLayer
 from neuralmonkey.nn.noisy_gru_cell import NoisyGRUCell
 from neuralmonkey.nn.pervasive_dropout_wrapper import PervasiveDropoutWrapper
 from neuralmonkey.checking import assert_type
@@ -41,18 +40,8 @@ class SentenceEncoder(object):
                                                       name="dropout")
             self.is_training = tf.placeholder(tf.bool, name="is_training")
 
-            self.inputs = [tf.placeholder(tf.int32, shape=[None],
-                                          name="input_{}".format(i))
-                           for i in range(max_input_len + 2)]
-
-            self.weight_ins = [tf.placeholder(tf.float32, shape=[None],
-                                              name="input_{}".format(i))
-                               for i in range(max_input_len + 2)]
-
-            self.weight_tensor = tf.concat(1, [tf.expand_dims(w, 1)
-                                               for w in self.weight_ins])
-
-            self.sentence_lengths = tf.to_int64(sum(self.weight_ins))
+            self.inputs = tf.placeholder(tf.int32, shape=[None, max_input_len + 2], name="encoder_inputs")
+            self.sentence_lengths = tf.placeholder(tf.int32, shape=[None], name="encoder_input_lengths")
 
             if parent_encoder:
                 self.word_embeddings = parent_encoder.word_embeddings
@@ -60,11 +49,8 @@ class SentenceEncoder(object):
                 self.word_embeddings = tf.Variable(tf.random_uniform(
                     [len(vocabulary), embedding_size], -1.0, 1.0))
 
-            embedded_inputs = [tf.nn.embedding_lookup(self.word_embeddings, i)
-                               for i in self.inputs]
-            dropped_embedded_inputs = [
-                tf.nn.dropout(i, self.dropout_placeholder)
-                for i in embedded_inputs]
+            embedded_inputs = tf.nn.embedding_lookup(self.word_embeddings, self.inputs)
+            dropped_embedded_inputs = tf.nn.dropout(embedded_inputs, self.dropout_placeholder)
 
             if parent_encoder:
                 self.forward_gru = parent_encoder.forward_gru
@@ -99,19 +85,12 @@ class SentenceEncoder(object):
                     self.backward_gru, backward_dropout_mask, scale)
 
 
+            outputs_bidi_t, encoded_t = tf.nn.bidirectional_dynamic_rnn(
+                self.forward_gru, self.backward_gru, 
+                dropped_embedded_inputs, self.sentence_lengths)
 
-
-            bidi_layer = BidirectionalRNNLayer(self.forward_gru,
-                                               self.backward_gru,
-                                               dropped_embedded_inputs,
-                                               self.sentence_lengths)
-
-            self.outputs_bidi = bidi_layer.outputs_bidi
-            self.encoded = bidi_layer.encoded
-
-            # attention tensor is of shape batch x time x (2*rnn_size)
-            self.attention_tensor = tf.concat(1, [tf.expand_dims(o, 1)
-                                                  for o in self.outputs_bidi])
+            self.attention_tensor = tf.concat(2, outputs_bidi_t)
+            self.encoded = tf.concat(1, encoded_t)
 
             self.attention_object = attention_type(
                 self.attention_tensor, scope="attention_{}".format(name),
@@ -129,16 +108,12 @@ class SentenceEncoder(object):
         res[self.sentence_lengths] = np.array(
             [min(self.max_input_len, len(s)) + 2 for s in sentences])
 
-        vectors, weights = self.vocabulary.sentences_to_tensor(
+        vectors, _ = self.vocabulary.sentences_to_tensor(
             sentences, self.max_input_len, train=train)
 
-        for words_plc, words_tensor in zip(self.inputs, vectors):
-            res[words_plc] = words_tensor
-
-        res[self.weight_ins[0]] = np.ones(len(sentences))
-
-        for weights_plc, weight_vector in zip(self.weight_ins[1:], weights):
-            res[weights_plc] = weight_vector
+        # as sentences_to_tensor returns list of shape (max-len+2) x batch,
+        # we need to transpose
+        res[self.inputs] = zip(*vectors)
 
         if train:
             res[self.dropout_placeholder] = self.dropout_keep_p
