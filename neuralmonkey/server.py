9d1436c30002356b30e1b8e3b6b302d32b69703d
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-11-07 21:41:51 +0100

    adjust server

--- neuralmonkey/server.py
+++ neuralmonkey/server.py
@@ -7,15 +7,14 @@ from flask import Flask, request
 
 from neuralmonkey.dataset import Dataset
 from neuralmonkey.learning_utils import run_on_dataset
-from neuralmonkey.checking import check_dataset_and_coders
-from neuralmonkey.run import initialize_for_running
+from neuralmonkey.run import CONFIG, initialize_for_running
 
 # tests: lint, mypy
 
 APP = Flask(__name__)
 APP.config.from_object(__name__)
 APP.config['args'] = None
-APP.config['sess'] = None
+
 
 @APP.route('/', methods=['GET', 'POST'])
 def post_request():
@@ -27,23 +26,23 @@ def post_request():
         code = 400
     else:
         args = APP.config['args']
-        sess = APP.config['sess']
 
         try:
             dataset = Dataset("request", request_data, {})
-            check_dataset_and_coders(dataset, args.encoders)
+            # TODO check the dataset
+            #check_dataset_and_coders(dataset, args.encoders)
 
-            result, _, _ = run_on_dataset(
-                [sess], args.runner, args.encoders + [args.decoder], args.decoder,
-                dataset, args.evaluation, args.postprocess, write_out=True)
-            response_data = {args.decoder.data_id: result}
+            _, response_data = run_on_dataset(
+                args.tf_manager, args.runners,
+                dataset, args.postprocess, write_out=False)
             code = 200
-        #pylint: disable=broad-except
+        # pylint: disable=broad-except
         except Exception as exc:
             response_data = {'error': str(exc)}
             code = 400
 
-    response_data['duration'] = (datetime.datetime.now() - start_time).total_seconds()
+    response_data['duration'] = (
+        datetime.datetime.now() - start_time).total_seconds()
     json_response = json.dumps(response_data)
     response = flask.Response(json_response,
                               content_type='application/json; charset=utf-8')
@@ -51,8 +50,10 @@ def post_request():
     response.status_code = code
     return response
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Runs Neural Monkey as a web server.")
+    parser = argparse.ArgumentParser(
+        description="Runs Neural Monkey as a web server.")
     parser.add_argument("--port", type=int, default=5000)
     parser.add_argument("--host", type=str, default="127.0.0.1")
     parser.add_argument("--configuration", type=str)
@@ -60,7 +61,8 @@ def main():
 
     print("")
 
-    args, sess = initialize_for_running(cli_args.configuration, None)
+    # pylint: disable=no-member
+    args = CONFIG.load_file(cli_args.configuration)
+    initialize_for_running(args.output, args.tf_manager, None)
     APP.config['args'] = args
-    APP.config['sess'] = sess[0]
     APP.run(port=cli_args.port, host=cli_args.host)
