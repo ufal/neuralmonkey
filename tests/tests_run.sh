1063ddd568ca6054c08f52b35d4fbcf972a1b6ea
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-11-10 21:43:21 +0100

    reorder tests

--- tests/tests_run.sh
+++ tests/tests_run.sh
@@ -7,14 +7,14 @@ bin/neuralmonkey-train tests/vocab.ini
 bin/neuralmonkey-train tests/small.ini
 bin/neuralmonkey-run tests/small.ini tests/test_data.ini
 
-rm -r tests/tmp-test-output
-
-bin/neuralmonkey-train tests/ensemble.ini
-bin/neuralmonkey-run tests/ensemble.ini tests/test_ensemble_data.ini
-
 bin/neuralmonkey-server --configuration=tests/small.ini --port=5000 &
 SERVER_PID=$!
 sleep 20
 
 curl 127.0.0.1:5000 -H "Content-Type: application/json" -X POST -d '{"source": ["I am the eggman.", "I am the walrus ."]}'
 kill $SERVER_PID
+
+rm -r tests/tmp-test-output
+
+bin/neuralmonkey-train tests/ensemble.ini
+bin/neuralmonkey-run tests/ensemble.ini tests/test_ensemble_data.ini
