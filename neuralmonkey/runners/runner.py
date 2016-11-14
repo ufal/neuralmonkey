055ffca0d9d07e91a87d126468c107540f31a96a
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-11-14 22:25:19 +0100

    naive ensembling into greedy runner

--- neuralmonkey/runners/runner.py
+++ neuralmonkey/runners/runner.py
@@ -1,12 +1,14 @@
 from typing import List, Tuple
+import numpy as np
 import tensorflow as tf
 
 from neuralmonkey.tf_manager import RunResult
 from neuralmonkey.runners.base_runner import BaseRunner, \
-        Executable, ExecutionResult
+    Executable, ExecutionResult
 
 # tests: mypy,pylint
 # pylint: disable=too-few-public-methods
+
 
 class GreedyRunner(BaseRunner):
 
@@ -19,13 +21,14 @@ class GreedyRunner(BaseRunner):
                       self._decoder.runtime_loss]
         else:
             losses = [tf.zeros([]), tf.zeros([])]
-        to_run = losses + self._decoder.decoded
+        to_run = losses + self._decoder.runtime_logprobs
         return GreedyRunExecutable(self._all_coders, to_run,
                                    self._decoder.vocabulary)
 
     @property
     def loss_names(self) -> List[str]:
         return ["train_xent", "runtime_xent"]
+
 
 class GreedyRunExecutable(Executable):
 
@@ -34,8 +37,6 @@ class GreedyRunExecutable(Executable):
         self.to_run = to_run
         self.vocabulary = vocabulary
 
-        self.loss_with_gt_ins = 0.0
-        self.loss_with_decoded_ins = 0.0
         self.decoded_sentences = []
         self.result = None  # type: Option[ExecutionResult]
 
@@ -44,16 +45,24 @@ class GreedyRunExecutable(Executable):
         return self.all_coders, self.to_run
 
     def collect_results(self, results: List[List[RunResult]]) -> None:
-        # TODO do ensembles
-        if len(results) > 1:
-            raise Exception('Does not support ensembling of multiple sessions')
+        train_loss = 0.
+        runtime_loss = 0.
+        summed_logprobs = [-np.inf for _ in range(len(self.to_run) - 2)]
 
-        sess_results = results[0]
+        for sess_result in results:
+            train_loss += sess_result[0]
+            runtime_loss += sess_result[1]
+
+            for i, logprob in enumerate(sess_result[2:]):
+                summed_logprobs[i] = np.logaddexp(summed_logprobs[i], logprob)
+
+        argmaxes = [np.argmax(l, axis=1) for l in summed_logprobs]
+
         decoded_sentences_batch = \
-            self.vocabulary.vectors_to_sentences(sess_results[2:])
+            self.vocabulary.vectors_to_sentences(argmaxes)
         self.result = ExecutionResult(
             outputs=decoded_sentences_batch,
-            losses=sess_results[:2],
+            losses=[train_loss, runtime_loss],
             scalar_summaries=None,
             histogram_summaries=None,
             image_summaries=None
