3a8cc48fef3bb9fd2940d77ce95e0f824c13f191
Jindra Helcl <j.helcl@sms.ed.ac.uk>
2016-11-22 19:11:14 +0000

    Add tests/bahdanau.ini to the tests scenarios.
    The scenario has reduced parameters so it should be faster. I would do the same for small.ini, which is not really that small...

--- tests/tests_run.sh
+++ tests/tests_run.sh
@@ -3,10 +3,10 @@
 set -ex
 
 bin/neuralmonkey-train tests/vocab.ini
+bin/neuralmonkey-train tests/bahdanau.ini
 
 bin/neuralmonkey-train tests/small.ini
 bin/neuralmonkey-run tests/small.ini tests/test_data.ini
-
 bin/neuralmonkey-server --configuration=tests/small.ini --port=5000 &
 SERVER_PID=$!
 sleep 20
@@ -14,4 +14,4 @@ sleep 20
 curl 127.0.0.1:5000 -H "Content-Type: application/json" -X POST -d '{"source": ["I am the eggman.", "I am the walrus ."]}'
 kill $SERVER_PID
 
-rm -r tests/tmp-test-output
+rm -rf tests/tmp-test-output
