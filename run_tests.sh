d4bc032a3150e55461fbbd6c6a82edfc30162164
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-12-01 10:45:52 +0100

    separate markdownlint tests suite

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,6 @@ set -ex
 
 tests/lint_run.sh
 tests/mypy_run.sh
+tests/mdl_run.sh
 tests/unit-tests_run.sh
 tests/tests_run.sh
--- tests/lint_run.sh
+++ tests/lint_run.sh
@@ -3,5 +3,3 @@
 set -e
 
 pylint -j 4 $(grep --include='*.py' --exclude='__init__.py' -rl neuralmonkey -e "^# *tests:.*lint")
-
-mdl $(find neuralmonkey -name '*.md') $(find tests -name '*.md') $(find examples -name '*.md')
