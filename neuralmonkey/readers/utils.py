dc878227e6cb7d5c1c848f04be437f194aeec93e
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-12-07 13:51:10 +0100

    forgotten file

---
+++ neuralmonkey/readers/utils.py
@@ -0,0 +1,12 @@
+from typing import List, Any, Callable
+import magic
+
+# tests: lint, mypy
+
+# pylint: disable=invalid-name
+Reader = Callable[[List[str]], Any]
+
+FILETYPER = magic.Magic(mime=True)
+FILETYPER.flags |= magic.MAGIC_SYMLINK
+FILETYPER.cookie = magic.magic_open(FILETYPER.flags)
+magic.magic_load(FILETYPER.cookie, None)
