f8704703c97716be28f8245bab39be503d7b7887
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2016-12-09 14:57:22 +0100

    fix funny-but-ugly-in-multiple-ways error in numpy reader

--- neuralmonkey/readers/numpy_reader.py
+++ neuralmonkey/readers/numpy_reader.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def numpy_reader(files: List[str]):
-    if len(list == 1):
+    if len(files) == 1:
         return np.load(files[0])
     else:
         np.concatenate([np.load(f) for f in files], axis=0)
