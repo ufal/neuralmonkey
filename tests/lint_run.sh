df0815670fe219134622c84789b572a783b9bea5
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-12-13 13:24:08 +0100

    seprate test suite for pycodestyle

--- tests/lint_run.sh
+++ tests/lint_run.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-pylint -j 4 $(find neuralmonkey -name '*.py')
-
 pycodestyle $(find neuralmonkey -name '*.py')
---
+++ tests/pycodestyle_run.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+
+set -e
+
+pylint -j 4 $(find neuralmonkey -name '*.py')
