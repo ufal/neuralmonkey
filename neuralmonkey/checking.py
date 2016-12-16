aaa5975cd125531e3d8ac7bb87c722acedeb6d51
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-12-16 15:57:03 +0100

    assert same shape

--- neuralmonkey/checking.py
+++ neuralmonkey/checking.py
@@ -97,6 +97,34 @@ def assert_shape(tensor: tf.Tensor,
         expected_str = ", ".join(
             "?" if x == -1 else str(x) for x in expected_shape)
         raise CheckingException(
-            "Shape mismatch in dimensions: {}. Shape was {} was [{}]".format(
-                ", ".join(str(d) for d in mismatching_dims),
-                shape_list, expected_str))
+            ("Shape mismatch of {} in dimensions: {}. "
+             "Shape was {}, but should be [{}]").format(
+                 tensor.name,
+                 ", ".join(str(d) for d in mismatching_dims),
+                 shape_list, expected_str))
+
+
+def assert_same_shape(tensor_a: tf.Tensor, tensor_b: tf.Tensor) -> None:
+    """Check if two tensors have the same shape."""
+
+    shape_a = tensor_a.get_shape().as_list()
+    shape_b = tensor_b.get_shape().as_list()
+
+    if len(shape_a) != len(shape_b):
+        raise CheckingException(
+            ("Tensor '{}' has {} dimensions and tensor '{}' has {} "
+             "dimension, but should have the same shape.").format(
+                 tensor_a.name, len(shape_a), tensor_b.name, len(shape_b)))
+
+    mismatching_dims = []
+    for i, (size_a, size_b) in enumerate(zip(shape_a, shape_b)):
+        if size_a != size_b:
+            mismatching_dims.append(i)
+
+    if mismatching_dims:
+        raise CheckingException(
+            ("Shape mismatch of '{}' and '{}' in dimensions: {}. "
+             "Shapes were {} and {}").format(
+                 tensor_a.name, tensor_b.name,
+                 ", ".join(str(d) for d in mismatching_dims),
+                 shape_a, shape_b))
