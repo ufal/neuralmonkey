fdbe7cd41801654e307d38140da570e30d0ccbac
Jindra Helcl <jindra.helcl@gmail.com>
2016-12-19 08:28:23 +0100

    Decorators (#191)
    * decorators module
    
    * Update decorators.py
    
    * Update decorators.py
    
    * addressing comments

---
+++ neuralmonkey/decorators.py
@@ -0,0 +1,15 @@
+# tests: lint
+
+from functools import wraps
+
+
+def tensor(func):
+    @wraps(func)
+    def decorate(self, *args, **kwargs):
+        attribute_name = "_{}_cached_placeholder".format(func.__name__)
+        if not hasattr(self, attribute_name):
+            value = func(self, *args, **kwargs)
+            setattr(self, attribute_name, value)
+        return getattr(self, attribute_name)
+
+    return property(decorate)
