0b564806310be149507e3c1ff7f2a4331e73ba34
Ondrej Bojar <bojar@ufal.mff.cuni.cz>
2017-01-02 15:19:29 +0100

    adding gorilla to readthedocs, let's see the result

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -133,12 +133,12 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'gorilla-1050384_960_720.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'gorilla.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
