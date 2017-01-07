069fd4b16a0cd6d703303db06b9c92135783a117
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2017-01-07 13:20:21 +0100

    Revert "Revert "change Variable to get_variable"" (with changes)
    This reverts commit 11b38aae8cdba1157daff784dca3e8fa5ddb6003.

--- neuralmonkey/decoders/decoder.py
+++ neuralmonkey/decoders/decoder.py
@@ -115,13 +115,14 @@ class Decoder(object):
             self._create_initial_state()
             self._create_embedding_matrix()
 
-            self.decoding_w = tf.Variable(
-                tf.random_uniform([rnn_size, len(vocabulary)], -0.5, 0.5),
-                name="state_to_word_W")
+            self.decoding_w = tf.get_variable(
+                "state_to_word_W", [rnn_size, len(self.vocabulary)],
+                initializer=tf.random_uniform_initializer(-0.5, 0.5))
 
-            self.decoding_b = tf.Variable(
-                tf.fill([len(vocabulary)], - math.log(len(vocabulary))),
-                name="state_to_word_b")
+            self.decoding_b = tf.get_variable(
+                "state_to_word_b", [len(self.vocabulary)],
+                initializer=tf.constant_initializer(
+                    - math.log(len(self.vocabulary))))
 
             # POSLEDNI TRAIN INPUT SE V DEKODOVACI FUNKCI NEPOUZIJE
             # (jen jako target)
@@ -260,10 +261,9 @@ class Decoder(object):
         """
         if self.embeddings_encoder is None:
             # TODO better initialization
-            self.embedding_matrix = tf.Variable(
-                tf.random_uniform([len(self.vocabulary), self.embedding_size],
-                                  -0.5, 0.5),
-                name="word_embeddings")
+            self.embedding_matrix = tf.get_variable(
+                "word_embeddings", [len(self.vocabulary), self.embedding_size],
+                initializer=tf.random_uniform_initializer(-0.5, 0.5))
         else:
             self.embedding_matrix = self.embeddings_encoder.embedding_matrix
 
