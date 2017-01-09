f7d2510fa7af9f1dbe86b54310e3c74d5818ac36
Ondrej Cifka <ondra@cifka.com>
2017-01-09 20:10:07 +0100

    Improve tf_save_images.py help

--- scripts/tf_save_images.py
+++ scripts/tf_save_images.py
@@ -1,18 +1,41 @@
 #!/usr/bin/env python3
-"""Extract a given image summary from an event file."""
+"""Extract a given image summary from an event file.
+
+Example usage:
+
+    %(prog)s myevents.out example/image --prefix example_
+
+This will extract the contents of the "example/image" summary from the event
+file "myevents.out" and write them into the files
+
+example_000000023940.png
+example_000000047900.png
+...
+
+where the numbers correspond to the steps associated with the images.
+
+The filenames can also be based on the order in which the images appear in the
+event file, e.g. "--suffix '{i:06d}.png'" will produce the files
+
+example_000000.png
+example_000001.png
+...
+
+"""
 
 import argparse
 import tensorflow as tf
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('event_file', metavar='event-file', help='the event file')
     parser.add_argument('tag', help='the image summary tag')
     parser.add_argument('--prefix', default='image_',
-                        help='the image filename prefix')
+                        help='the image filename prefix (default: %(default)s)')
     parser.add_argument('--suffix', default='{step:012d}.png',
-                        help='the image filename suffix formatting string')
+                        help='the image filename suffix formatting string '
+                        '(default: %(default)s)')
     args = parser.parse_args()
 
     i = 0
