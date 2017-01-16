df7235e13c14f13dd27ede6c098a9b5b80b4b297
Ondrej Cifka <ondra@cifka.com>
2017-01-16 17:08:01 +0100

    Add test_functions

---
+++ neuralmonkey/tests/test_functions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Unit tests for functions.py."""
+# tests: mypy, lint
+
+import unittest
+import tensorflow as tf
+
+from neuralmonkey.functions import piecewise_function
+
+
+class TestPiecewiseFunction(unittest.TestCase):
+
+    def test_piecewise_constant(self):
+        x = tf.placeholder(dtype=tf.int32)
+        y = piecewise_function(x, [-0.5, 1.2, 3, 2], [-1, 2, 1000],
+                               dtype=tf.float32)
+
+        with tf.Session() as sess:
+            self.assertAlmostEqual(sess.run(y, {x: -2}), -0.5)
+            self.assertAlmostEqual(sess.run(y, {x: -1}), 1.2)
+            self.assertAlmostEqual(sess.run(y, {x: 999}), 3)
+            self.assertAlmostEqual(sess.run(y, {x: 1000}), 2)
+            self.assertAlmostEqual(sess.run(y, {x: 1001}), 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
