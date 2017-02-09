715be76d4f2ab2d5235f79b7bfadf204c8130964
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2017-02-09 14:56:08 +0100

    fix ter avg, closes #307

--- neuralmonkey/evaluators/ter.py
+++ neuralmonkey/evaluators/ter.py
@@ -9,14 +9,16 @@ class TEREvalutator(object):
 
     def __call__(self, decoded, references):
         ter_sum = 0
+        count = 0
         for hyp, ref in zip(decoded, references):
+            count += 1
             if ref and hyp:
                 ter_sum += pyter.ter(hyp, ref)
             elif not ref and not hyp:
                 ter_sum += 0.
             else:
                 ter_sum += 1.
-        return ter_sum / len(decoded)
+        return ter_sum / count
 
 
 TER = TEREvalutator()
