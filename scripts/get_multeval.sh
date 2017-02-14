9ab87bf874248a0b40fb1a5d281b70c727c0f763
Julia Kreutzer <juliakreutzer1@aol.com>
2017-02-14 21:11:22 +0100

    installation scripts for multeval and beer

---
+++ scripts/get_multeval.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Get multeval scripts
+# See: https://github.com/jhclark/multeval
+
+wget http://www.cs.cmu.edu/~jhclark/downloads/multeval-0.5.1.tgz
+tar -xvzf multeval-0.5.1.tgz
+
+cp multeval-0.5.1/constants ../
