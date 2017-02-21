d0da9eba3f44099583dae549615a6bd9270a4942
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2017-02-21 16:20:13 +0100

    move example data to ufallab/~helcl

--- examples/data/translation/download_example_data.sh
+++ examples/data/translation/download_example_data.sh
@@ -1,7 +1,5 @@
-wget https://www.dropbox.com/s/5wvl5d6g2pvwkiz/bpe_merges
-
-wget https://www.dropbox.com/s/qqwxfc14z7umvyc/train.en
-wget https://www.dropbox.com/s/gfqoidimjz8weu0/train.de
-
-wget https://www.dropbox.com/s/aiclmky0k1vqxrb/val.de
-wget https://www.dropbox.com/s/qmaxob2w963lmnh/val.en
+wget http://ufallab.ms.mff.cuni.cz/~helcl/neuralmonkey-example-data/translation/bpe_merges
+wget http://ufallab.ms.mff.cuni.cz/~helcl/neuralmonkey-example-data/translation/train.en
+wget http://ufallab.ms.mff.cuni.cz/~helcl/neuralmonkey-example-data/translation/train.de
+wget http://ufallab.ms.mff.cuni.cz/~helcl/neuralmonkey-example-data/translation/val.de
+wget http://ufallab.ms.mff.cuni.cz/~helcl/neuralmonkey-example-data/translation/val.en
