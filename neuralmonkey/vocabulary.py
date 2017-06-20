15bd3efc6448766325e84e0633243ce1196b9085
Tom Kocmi <kocmitom@gmail.com>
2017-06-20 10:55:04 +0200

    changing parameters of fnc call

--- neuralmonkey/vocabulary.py
+++ neuralmonkey/vocabulary.py
@@ -43,7 +43,7 @@ def _is_special_token(word: str) -> bool:
             or word == UNK_TOKEN)
 
 
-def from_file(path: str) -> 'Vocabulary':
+def from_file(*args, **kwargs) -> 'Vocabulary':
     raise NotImplementedError("Use loading by from_wordlist")
 
 
