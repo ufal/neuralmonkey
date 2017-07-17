b51bd4f70b1a6a0b11552c6e024b67d85e180b71
Tom Kocmi <kocmitom@gmail.com>
2017-07-17 09:36:13 +0200

    helper class

--- neuralmonkey/encoders/conv_s2s_encoder.py
+++ neuralmonkey/encoders/conv_s2s_encoder.py
@@ -9,12 +9,14 @@ from typeguard import check_argument_types
 
 from neuralmonkey.encoders.attentive import Attentive
 from neuralmonkey.model.model_part import ModelPart, FeedDict
-from neuralmonkey.model.sequence import EmbeddedFactorSequence
 from neuralmonkey.logging import log
 from neuralmonkey.dataset import Dataset
 from neuralmonkey.decorators import tensor
 from neuralmonkey.nn.projection import glu, linear
 from neuralmonkey.nn.utils import dropout
+from neuralmonkey.vocabulary import Vocabulary
+from neuralmonkey.model.sequence import (EmbeddedFactorSequence,
+                                         EmbeddedSequence)
 
 
 class ConvolutionalSentenceEncoder(ModelPart, Attentive):
@@ -46,7 +48,7 @@ class ConvolutionalSentenceEncoder(ModelPart, Attentive):
         self.kernel_width = kernel_width
         self.dropout_keep_prob = dropout_keep_prob
 
-        # because of the convolutions it must be fixed
+        # because of the convolutions must be fixed
         self.input_sequence.pad_to_max_len = True
 
         if conv_features <= 0:
@@ -94,8 +96,8 @@ class ConvolutionalSentenceEncoder(ModelPart, Attentive):
         # initialization in the same way as in original CS2S implementation
         with tf.variable_scope("input_projection"):
             return tf.get_variable(
-                "order_embeddings", [self.input_sequence.max_length, 
-                    self.input_sequence.embedding_sizes[0]],
+                "order_embeddings", [self.input_sequence.max_length,
+                                     self.input_sequence.embedding_sizes[0]],
                 initializer=tf.random_normal_initializer(stddev=0.1))
 
     @tensor
@@ -134,3 +136,57 @@ class ConvolutionalSentenceEncoder(ModelPart, Attentive):
         fd[self.train_mode] = train
 
         return fd
+
+
+class SentenceEncoder(ConvolutionalSentenceEncoder):
+    # pylint: disable=too-many-arguments,too-many-locals
+    def __init__(self,
+                 name: str,
+                 vocabulary: Vocabulary,
+                 data_id: str,
+                 embedding_size: int,
+                 conv_features: int,
+                 encoder_layers: int,
+                 kernel_width: int = 5,
+                 max_input_len: int = None,
+                 dropout_keep_prob: float = 1.0,
+                 attention_type: type = None,
+                 attention_fertility: int = 3,
+                 attention_state_size: int = None,
+                 save_checkpoint: str = None,
+                 load_checkpoint: str = None) -> None:
+        """Create a new instance of the sentence encoder. """
+
+        # TODO Think this through.
+        s_ckp = "input_{}".format(save_checkpoint) if save_checkpoint else None
+        l_ckp = "input_{}".format(load_checkpoint) if load_checkpoint else None
+
+        # TODO! Representation runner needs this. It is not simple to do it in
+        # recurrent encoder since there may be more source data series. The
+        # best way could be to enter the data_id parameter manually to the
+        # representation runner
+        self.data_id = data_id
+
+        input_sequence = EmbeddedSequence(
+            name="{}_input".format(name),
+            vocabulary=vocabulary,
+            data_id=data_id,
+            embedding_size=embedding_size,
+            max_length=max_input_len,
+            save_checkpoint=s_ckp,
+            load_checkpoint=l_ckp)
+
+        ConvolutionalSentenceEncoder.__init__(
+            self,
+            name=name,
+            input_sequence=input_sequence,
+            conv_features=conv_features,
+            encoder_layers=encoder_layers,
+            kernel_width=kernel_width,
+            dropout_keep_prob=dropout_keep_prob,
+            attention_type=attention_type,
+            attention_fertility=attention_fertility,
+            attention_state_size=attention_state_size,
+            save_checkpoint=save_checkpoint,
+            load_checkpoint=load_checkpoint)
+    # pylint: enable=too-many-arguments,too-many-locals
