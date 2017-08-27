ccbbdf604f4b3c8109da6e1d7a2ab570e4e9a309
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2017-08-27 18:03:21 +0200

    [ci skip] gorila images in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -134,12 +134,12 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'gorilla-logo.png'
+html_logo = 'img/gorilla-logo.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'gorilla.ico'
+html_favicon = 'img/gorilla.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
