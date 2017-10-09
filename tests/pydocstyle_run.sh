f1dc50b3397a957918e7e3845d9dc6a37f196e29
Dusan Varis <varis@ufal.mff.cuni.cz>
2017-10-09 16:24:32 +0200

    added pydocstyle test to .travis.yml

--- tests/pydocstyle_run.sh
+++ tests/pydocstyle_run.sh
@@ -9,6 +9,6 @@ IGNORED="D202,D203,D213,D406,D407,D408,D409,D413"
 # These are currently turned off on master branch
 # because of the missing docstrings. However, they should
 # be switched on in the future.
-IGNORED="D100,D101,D102,D103,D104,$IGNORED"
+IGNORED="D100,D101,D102,D103,D104,D107,$IGNORED"
 
 pydocstyle --ignore=$IGNORED neuralmonkey
