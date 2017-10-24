aec377b375c68ae06d348cc7bba7f9a233b6d896
Dusan Varis <varis@ufal.mff.cuni.cz>
2017-10-24 16:52:34 +0200

    Pylint no reports

--- tests/lint_run.sh
+++ tests/lint_run.sh
@@ -3,4 +3,4 @@
 set -Ex
 trap 'echo -e "\033[1;31mPylint spotted errors!\033[0m"' ERR
 
-pylint --output-format=colorized -j 4 --load-plugins pylint_quotes neuralmonkey
+pylint --output-format=colorized -j 4 --reports=no --load-plugins pylint_quotes neuralmonkey
