ee0ec89e939a6fb4518bceb81857fdb1d7a7fb64
Ondrej Cifka <ondra@cifka.com>
2018-02-01 22:39:31 +0100

    Add glob test

--- neuralmonkey/tests/test_dataset.py
+++ neuralmonkey/tests/test_dataset.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3.5
 
+import os
+import tempfile
 import unittest
 
-from neuralmonkey.dataset import LazyDataset
+from neuralmonkey.dataset import LazyDataset, from_files
 from neuralmonkey.readers.plain_text_reader import UtfPlainTextReader
 
 
@@ -14,6 +16,20 @@ class TestDataset(unittest.TestCase):
 
         with self.assertRaises(FileNotFoundError):
             LazyDataset("name", paths_and_readers, {}, None)
+
+    def test_glob(self):
+        filenames = sorted(["abc1", "abc2", "abcxx", "xyz"])
+        contents = ["a", "b", "c", "d"]
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            for fname, text in zip(filenames, contents):
+                with open(os.path.join(tmp_dir, fname), "w") as file:
+                    print(text, file=file)
+
+            dataset = from_files(
+                s_data=[os.path.join(tmp_dir, "abc?"),
+                        os.path.join(tmp_dir, "xyz*")])
+
+            self.assertEqual(dataset.get_series("data"), [["a"], ["b"], ["d"]])
 
 
 if __name__ == "__main__":
