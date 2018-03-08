236fbd4d35b7018849380e39ae2ff80aa628dba0
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2018-03-08 16:09:07 +0100

    get rid six, review comments

--- scripts/avg_checkpoints.py
+++ scripts/avg_checkpoints.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-"""Script to average values of variables in a list of checkpoint files.
+"""Compute the average of each variable in a list of checkpoint files.
 
-Given a list of model checkpoints, it generates a new checkpoint wiht
-parameters which are an arithmetic average of them.
+Given a list of model checkpoints, it generates a new checkpoint with
+parameters which are the arithmetic average of them.
 
-This is based on a script from Tensor2Tensor:
+Based on a script from Tensor2Tensor:
 https://github.com/tensorflow/tensor2tensor/blob/master/tensor2tensor/utils/avg_checkpoints.py
 """
 
 import argparse
 import os
+import re
 
 import numpy as np
-import six
-from six.moves import zip    # pylint: disable=redefined-builtin
 import tensorflow as tf
 
 from neuralmonkey.logging import log
+
+
+IGNORED_PATTERNS = ["global_step"]
 
 
 def main() -> None:
@@ -37,19 +39,19 @@ def main() -> None:
                 ", ".join(non_existing_chckpoints)))
 
     # Read variables from all checkpoints and average them.
-    log("Get list of variables:")
+    log("Getting list of variables")
     var_list = tf.contrib.framework.list_variables(args.checkpoints[0])
     var_values, var_dtypes = {}, {}
     for (name, shape) in var_list:
-        if not name.startswith("global_step"):
+        if not any(re.match(pat, name) for pat in IGNORED_PATTERNS):
             var_values[name] = np.zeros(shape)
     for checkpoint in args.checkpoints:
+        log("Reading from checkpoint {}".format(checkpoint))
         reader = tf.contrib.framework.load_checkpoint(checkpoint)
         for name in var_values:
             tensor = reader.get_tensor(name)
             var_dtypes[name] = tensor.dtype
             var_values[name] += tensor
-        log("Read from checkpoint {}".format(checkpoint))
     for name in var_values:    # Average.
         var_values[name] /= len(args.checkpoints)
 
@@ -61,13 +63,13 @@ def main() -> None:
     assign_ops = [tf.assign(v, p) for (v, p) in zip(tf_vars, placeholders)]
     global_step = tf.Variable(
             0, name="global_step", trainable=False, dtype=tf.int64)
-    saver = tf.train.Saver(tf.all_variables())
+    saver = tf.train.Saver()
 
     # Build a model only with variables, set them to the average values.
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for p, assign_op, (name, value) in zip(placeholders, assign_ops,
-                                               six.iteritems(var_values)):
+                                               var_values.items()):
             sess.run(assign_op, {p: value})
         saver.save(sess, args.output_path, global_step=global_step)
 
