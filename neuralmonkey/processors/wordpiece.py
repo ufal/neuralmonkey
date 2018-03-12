ca6b5fd66c22e44c535c2715aec28af5bfcf737e
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2018-03-12 21:37:05 +0100

    Refactor for review

--- neuralmonkey/processors/wordpiece.py
+++ neuralmonkey/processors/wordpiece.py
@@ -1,89 +1,83 @@
+"""Loose reimplementation of the t2t tokenizer.
 
+Original code:
+https://github.com/tensorflow/tensor2tensor/blob/v1.5.5/tensor2tensor/data_generators/tokenizer.py
+"""
 from typing import List
 import sys
 import unicodedata
-import six
 
 from neuralmonkey.logging import log
 from neuralmonkey.vocabulary import Vocabulary
 
-# pylint: disable=too-few-public-methods
 
 ALNUM_CHAR_SET = set(
-    six.unichr(i) for i in range(sys.maxunicode)
-    if (unicodedata.category(six.unichr(i)).startswith("L") or
-        unicodedata.category(six.unichr(i)).startswith("N")))
+    chr(i) for i in range(sys.maxunicode)
+    if (unicodedata.category(chr(i)).startswith("L") or
+        unicodedata.category(chr(i)).startswith("N")))
 
 
-class WordpiecePreprocessor(object):
-    """Loose implementation of the t2t SubwordTextTokenizer.
+def wordpiece_encode(sentence: List[str], vocabulary: Vocabulary) -> List[str]:
+    sent = " ".join(sentence)
+    tokens_str = sent[0]
+    for i in range(1, len(sent)):
+        if sent[i] == " ":
+            continue
 
-    Paper: TODO?
-    Code: TODO
-    """
-
-    def __init__(self,
-                 vocabulary: Vocabulary) -> None:
-        log("Initializing wordpiece preprocessor")
-
-        self.vocabulary = vocabulary
-
-    def __call__(self, sentence: List[str]) -> List[str]:
-        """See the code.
-
-        TODO
-        """
-
-        sent = " ".join(sentence)
-        tokens_str = sent[0]
-        for i in range(1, len(sent)):
-            if sent[i] == " ":
-                continue
-
-            if ((sent[i] in ALNUM_CHAR_SET) != (sent[i - 1] in ALNUM_CHAR_SET)
-                    or sent[i - 1] == " "):
-                tokens_str += " "
+        if ((sent[i] in ALNUM_CHAR_SET) != (sent[i - 1] in ALNUM_CHAR_SET)
+                or sent[i - 1] == " "):
+            tokens_str += " "
             tokens_str += sent[i]
 
-        # Mark the end of each token
-        # TODO: escape the characters properly
-        tokens = [tok + "_" for tok in tokens_str.split(" ")]
+    # Mark the end of each token
+    # TODO (#669): escape the characters properly
+    tokens = [tok + "_" for tok in tokens_str.split(" ")]
 
-        output = []  # type: List[str]
-        for tok in tokens:
-            tok_start = 0
-            ret = []
-            while tok_start < len(tok):
-                for tok_end in range(len(tok), tok_start, -1):
-                    subtoken = tok[tok_start:tok_end]
-                    if subtoken in self.vocabulary:
-                        ret.append(subtoken)
-                        tok_start = tok_end
-                        break
-                else:
-                    raise ValueError(
-                        "Subword '{}' (from {}) is not in the vocabulary"
-                        .format(tok[tok_start:len(tok)], tok))
-            output += ret
-
-        return output
-
-
-class WordpiecePostprocessor(object):
-
-    # pylint: disable=no-self-use
-    def __init__(self) -> None:
-        pass
-
-    def __call__(self, decoded_sentences: List[List[str]]) -> List[List[str]]:
-        return [self.decode(s) for s in decoded_sentences]
-
-    def decode(self, sentence: List[str]) -> List[str]:
-        output = []
-        for tok in sentence:
-            if tok.endswith("_"):
-                output.append(tok[:-1] + " ")
+    output = []  # type: List[str]
+    for tok in tokens:
+        tok_start = 0
+        ret = []
+        while tok_start < len(tok):
+            for tok_end in range(len(tok), tok_start, -1):
+                subtoken = tok[tok_start:tok_end]
+                if subtoken in vocabulary:
+                    ret.append(subtoken)
+                    tok_start = tok_end
+                    break
             else:
-                output.append(tok)
+                raise ValueError(
+                    "Subword '{}' (from '{}') is not in the vocabulary"
+                    .format(tok[tok_start:len(tok)], tok))
+        output += ret
 
-        return "".join(output).rstrip(" ").split(" ")
+    return output
+
+
+def wordpiece_decode(sentence: List[str]) -> List[str]:
+    output = []
+    for tok in sentence:
+        if tok.endswith("_"):
+            output.append(tok[:-1] + " ")
+        else:
+            output.append(tok)
+
+    return "".join(output).rstrip(" ").split(" ")
+
+
+def wordpiece_decode_batch(sentences: List[List[str]]) -> List[List[str]]:
+    return [wordpiece_decode(s) for s in sentences]
+
+
+def get_wordpiece_preprocessor(
+        vocabulary: Vocabulary) -> Callable[[List[str]], List[str]]:
+    check_argument_types()
+    return lambda s: wordpiece_encode(s, vocabulary)
+
+
+# pylint: disable=invalid-name
+# Syntactic sugar for configuration
+
+WordpiecePreprocessor = get_wordpiece_preprocessor
+WordpiecePostprocessor = lambda: wordpiece_decode_batch
+
+DefaultWordpiecePostprocessor = wordpiece_decode_batch
