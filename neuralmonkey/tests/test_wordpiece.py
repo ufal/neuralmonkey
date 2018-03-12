433627f5947022eb38afe700955468dd4dba3fbe
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2018-03-13 00:41:32 +0100

    nicer wordpiece unit test and dealing with <unk>s

--- neuralmonkey/tests/test_wordpiece.py
+++ neuralmonkey/tests/test_wordpiece.py
@@ -17,41 +17,74 @@ TOKENIZED_CORPUS = [[a + "_" for a in s.split()] for s in CORPUS]
 
 # Create list of characters required to process the CORPUS with wordpieces
 CORPUS_CHARS = [x for c in set("".join(CORPUS)) for x in [c, c + "_"]]
-VOCABULARY = Vocabulary()
-
-for w in CORPUS_CHARS:
-    VOCABULARY.add_word(w)
-
-for sent in TOKENIZED_CORPUS:
-    VOCABULARY.add_tokenized_text(sent)
-
-
-PREPROCESSOR = WordpiecePreprocessor(VOCABULARY)
-POSTPROCESSOR = WordpiecePostprocessor
+ESCAPE_CHARS = "\\_u0987654321;"
+C_CARON = "\\269;"
+A_ACUTE = "225"
 
 
 class TestWordpieces(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        vocabulary = Vocabulary()
+
+        for c in CORPUS_CHARS + list(ESCAPE_CHARS):
+            vocabulary.add_word(c)
+
+        for sent in TOKENIZED_CORPUS:
+            vocabulary.add_tokenized_text(sent)
+
+        vocabulary.add_word(C_CARON)
+        vocabulary.add_word(A_ACUTE)
+
+        cls.preprocessor = WordpiecePreprocessor(vocabulary)
+        cls.postprocessor = WordpiecePostprocessor
 
     def test_preprocess_ok(self):
         raw = "I am the walrus".split()
         gold = "I_ am_ the_ walrus_".split()
 
-        preprocessed = PREPROCESSOR(raw)
+        preprocessed = TestWordpieces.preprocessor(raw)
         self.assertSequenceEqual(preprocessed, gold)
 
     def test_preprocess_split(self):
         raw = "Ich bin der walrus".split()
         gold = "I c h_ b i n_ d e r_ walrus_".split()
 
-        preprocessed = PREPROCESSOR(raw)
+        preprocessed = TestWordpieces.preprocessor(raw)
         self.assertSequenceEqual(preprocessed, gold)
 
-    # TODO (#669): test encoding of UNK char/word
-    def process_unk(self):
-        pass
+    def test_preprocess_unk(self):
+        raw = "Ich bin der čermák".split()
+        gold = "I c h_ b i n_ d e r_ \\269; e r m \\ 225 ; k_".split()
+
+        preprocessed = TestWordpieces.preprocessor(raw)
+        self.assertSequenceEqual(preprocessed, gold)
+
+    def test_postprocess_ok(self):
+        output = "I_ am_ the_ walrus_".split()
+        gold = ["I am the walrus".split()]
+
+        postprocessed = TestWordpieces.postprocessor([output])
+        self.assertSequenceEqual(postprocessed, gold)
+
+    def test_postprocess_split(self):
+        output = "I c h_ b i n_ d e r_ walrus_".split()
+        gold = ["Ich bin der walrus".split()]
+
+        postprocessed = TestWordpieces.postprocessor([output])
+        self.assertSequenceEqual(postprocessed, gold)
+
+    def test_postprocess_unk(self):
+        output = "I c h_ b i n_ d e r_ \\269; e r m \\ 225 ; k_".split()
+        gold = ["Ich bin der čermák".split()]
+
+        postprocessed = TestWordpieces.postprocessor([output])
+        self.assertSequenceEqual(postprocessed, gold)
 
     # TODO (#669): implement wordpiece generator
-    def create_wordpieces(self):
+    @unittest.skip("not implemented yet")
+    def test_make_wordpieces(self):
         pass
 
 
