88781e99c4a285fd2f1d9dcc7d841d73cf925896
Dusan Varis <varis@ufal.mff.cuni.cz>
2018-04-12 17:49:45 +0200

    added main.batchime_scheme and bucketed batching

--- neuralmonkey/learning_utils.py
+++ neuralmonkey/learning_utils.py
@@ -38,6 +38,7 @@ def training_loop(tf_manager: TensorFlowManager,
                   epochs: int,
                   trainer: Union[Trainer, List[Trainer]],
                   batch_size: int,
+                  batching_scheme: str,
                   log_directory: str,
                   evaluators: EvalConfiguration,
                   runners: List[BaseRunner],
@@ -62,6 +63,7 @@ def training_loop(tf_manager: TensorFlowManager,
         trainer: The trainer object containg the TensorFlow code for computing
             the loss and optimization operation.
         batch_size: number of examples in one mini-batch
+        batching_scheme: The units of measure when using batch_size
         log_directory: Directory where the TensordBoard log will be generated.
             If None, nothing will be done.
         evaluators: List of evaluators. The last evaluator is used as the main.
