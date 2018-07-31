5dec44f15585da50363b2518ebd5c75834384f73
Dusan Varis <varis@ufal.mff.cuni.cz>
2018-07-31 18:36:14 +0200

    addressing PR reviews

--- neuralmonkey/trainers/regularizers.py
+++ neuralmonkey/trainers/regularizers.py
@@ -1,7 +1,7 @@
 """Variable regularizers.
 
 This module contains classes that can be used as a variable regularizers
-during training. All implementation should be derived from the BaseRegularizer
+during training. All implementation should be derived from the Regularizer
 class.
 
 """
@@ -14,12 +14,18 @@ from typeguard import check_argument_types
 from neuralmonkey.logging import log
 
 
-class BaseRegularizer:
+class Regularizer:
     """Base class for the regularizers."""
 
     def __init__(self,
                  name: str,
                  weight: float) -> None:
+        """Create the regularizer.
+
+        Arguments:
+            name: Regularizer name.
+            weight: Weight of the regularization term.
+        """
         check_argument_types()
 
         self._name = name
@@ -37,32 +43,49 @@ class BaseRegularizer:
         raise NotImplementedError("Abstract method")
 
 
-class L1Regularizer(BaseRegularizer):
+class L1Regularizer(Regularizer):
+    """L1 regularizer."""
 
     def __init__(self,
                  name: str = "train_l1",
                  weight: float = 1.0e-8) -> None:
-        BaseRegularizer.__init__(self, name, weight)
+        """Create the regularizer.
+
+        Arguments:
+            name: Regularizer name.
+            weight: Weight of the regularization term.
+        """
+        Regularizer.__init__(self, name, weight)
 
     def value(self, variables: List[tf.Tensor]) -> float:
         return sum(tf.reduce_sum(abs(v)) for v in variables)
 
 
-class L2Regularizer(BaseRegularizer):
+class L2Regularizer(Regularizer):
+    """L2 regularizer."""
 
     def __init__(self,
                  name: str = "train_l2",
                  weight: float = 1.0e-8) -> None:
-        BaseRegularizer.__init__(self, name, weight)
+        """Create the regularizer.
+
+        Arguments:
+            name: Regularizer name.
+            weight: Weight of the regularization term.
+        """
+        Regularizer.__init__(self, name, weight)
 
     def value(self, variables: List[tf.Tensor]) -> float:
         return sum(tf.reduce_sum(v ** 2) for v in variables)
 
 
-class EWCRegularizer(BaseRegularizer):
+class EWCRegularizer(Regularizer):
     """Regularizer based on the Elastic Weight Consolidation.
 
-    TODO description
+    Implements Elastic Weight Consolidation from the "Overcoming catastrophic
+    forgetting in neural networks" paper.
+
+    https://arxiv.org/pdf/1612.00796.pdf
     """
 
     def __init__(self,
@@ -70,9 +93,19 @@ class EWCRegularizer(BaseRegularizer):
                  weight: float = 0.,
                  gradients_file: str = None,
                  variables_file: str = None) -> None:
+        """Create the regularizer.
+
+        Arguments:
+            name: Regularizer name.
+            weight: Weight of the regularization term.
+            gradients_file: File containing the gradient estimates
+                from the previous task.
+            variables_files: File containing the variables learned
+                on the previous task.
+        """
         check_argument_types()
 
-        BaseRegularizer.__init__(self, name, weight)
+        Regularizer.__init__(self, name, weight)
 
         if gradients_file is None:
             raise ValueError("Missing gradients_file")
@@ -88,13 +121,16 @@ class EWCRegularizer(BaseRegularizer):
         log("Gradient estimates loaded")
 
     def value(self, variables: List[tf.Tensor]) -> float:
-        ewc_value = 0.0
+        ewc_value = tf.constant(0.0)
         for var in variables:
             var_name = var.name.split(":")[0]
-            init_var = self.init_vars.get_tensor(var_name)
-            gradient = self.gradients[var_name]
-            ewc_value += tf.reduce_sum(tf.multiply(
-                tf.square(gradient), tf.square(var - init_var)))
+            if (var_name in self.gradients.files
+                    and self.init_vars.has_tensor(var_name)):
+                init_var = self.init_vars.get_tensor(var_name)
+                gradient = tf.constant(
+                    self.gradients[var_name], name="ewc_gradients")
+                ewc_value += tf.reduce_sum(tf.multiply(
+                    tf.square(gradient), tf.square(var - init_var)))
 
         return ewc_value
 
