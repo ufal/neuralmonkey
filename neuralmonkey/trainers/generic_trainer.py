c75dda12c1931adfa631ed16b42caf80d43ba51c
Dusan Varis <varis@ufal.mff.cuni.cz>
2018-08-08 22:06:22 +0200

    fixed pylints in generic_trainer, fixed typos

--- neuralmonkey/trainers/generic_trainer.py
+++ neuralmonkey/trainers/generic_trainer.py
@@ -10,7 +10,8 @@ from neuralmonkey.model.feedable import Feedable
 from neuralmonkey.runners.base_runner import GraphExecutor, NextExecute
 from neuralmonkey.trainers.objective import (
     Objective, Gradients, ObjectiveWeight)
-from neuralmonkey.trainers.regularizers import (Regularizer, L2Regularizer)
+from neuralmonkey.trainers.regularizers import (
+    Regularizer, L1Regularizer, L2Regularizer)
 
 BIAS_REGEX = re.compile(r"[Bb]ias")
 
@@ -74,6 +75,8 @@ class GenericTrainer(GraphExecutor, Feedable):
         self.var_collection = var_collection
         if self.var_collection is None:
             self.var_collection = tf.GraphKeys.TRAINABLE_VARIABLES
+
+        self.objectives = objectives
 
         self.regularizers = []  # type: List[Regularizer]
         if regularizers is not None:
@@ -214,10 +217,15 @@ class GenericTrainer(GraphExecutor, Feedable):
 
         reg_values = self.regularization_losses
         # we always want to include l2 values in the summary
+        if L1Regularizer not in [type(r) for r in self.regularizers]:
+            l1_reg = L1Regularizer(name="train_l1", weight=0.)
+            tf.summary.scalar(l1_reg.name, l1_reg.value(regularizable),
+                              collections=["summary_train"])
         if L2Regularizer not in [type(r) for r in self.regularizers]:
             l2_reg = L2Regularizer(name="train_l2", weight=0.)
             tf.summary.scalar(l2_reg.name, l2_reg.value(regularizable),
                               collections=["summary_train"])
+
         for reg, reg_value in zip(self.regularizers, reg_values):
             tf.summary.scalar(reg.name, reg_value,
                               collections=["summary_train"])
--- neuralmonkey/trainers/regularizers.py
+++ neuralmonkey/trainers/regularizers.py
@@ -15,11 +15,11 @@ from neuralmonkey.logging import log
 
 
 class Regularizer(metaclass=ABCMeta):
-    """Base clas    s for regularizers.
+    """Base class for regularizers.
 
     Regularizer objects are used to introduce additional loss terms to
-    the trainerthus constraining the model variable during training. These
-    loss terms have an adjustable weight allowing to set the ``importance''
+    the trainer, thus constraining the model variable during training. These
+    loss terms have an adjustable weight allowing to set the "importance"
     of the term.
     """
 
@@ -31,7 +31,7 @@ class Regularizer(metaclass=ABCMeta):
         Arguments:
             name: Regularizer name.
             weight: Weight of the regularization term (usually expressed
-                 as ``lambda'' in the literature).
+                 as "lambda" in the literature).
         """
         self._name = name
         self._weight = weight
@@ -64,7 +64,7 @@ class L1Regularizer(Regularizer):
 
         Arguments:
             name: Regularizer name.
-            weight: Weight of the regularization term (default=1.0e-8.
+            weight: Weight of the regularization term.
         """
         Regularizer.__init__(self, name, weight)
 
@@ -95,9 +95,8 @@ class EWCRegularizer(Regularizer):
 
     Implements Elastic Weight Consolidation from the "Overcoming catastrophic
     forgetting in neural networks" paper.
-    The regularizer applies separate regularization weight to each trainable
-    variable based on how important the variable was for the previously
-    learned task.
+    The regularizer applies a separate regularization weight to each trainable
+    variable based on its importance for the previously learned task.
 
     https://arxiv.org/pdf/1612.00796.pdf
     """
@@ -120,8 +119,8 @@ class EWCRegularizer(Regularizer):
         check_argument_types()
         Regularizer.__init__(self, name, weight)
 
-        log("Loading initial variables for EWC from "
-            "{}.".format(variables_file))
+        log("Loading initial variables for EWC from {}."
+            .format(variables_file))
         self.init_vars = tf.contrib.framework.load_checkpoint(variables_file)
         log("EWC initial variables loaded.")
 
@@ -132,15 +131,14 @@ class EWCRegularizer(Regularizer):
     def value(self, variables: List[tf.Tensor]) -> tf.Tensor:
         ewc_value = tf.constant(0.0)
         for var in variables:
-            var_name = var.name
-            init_var_name = var_name.split(":")[0]
-            if (var_name in self.gradients.files
+            init_var_name = var.name.split(":")[0]
+            if (var.name in self.gradients.files
                     and self.init_vars.has_tensor(init_var_name)):
                 init_var = tf.constant(
                     self.init_vars.get_tensor(init_var_name),
                     name="{}_init_value".format(init_var_name))
                 grad_squared = tf.constant(
-                    np.square(self.gradients[var_name]),
+                    np.square(self.gradients[var.name]),
                     name="{}_ewc_weight".format(init_var_name))
                 ewc_value += tf.reduce_sum(tf.multiply(
                     grad_squared, tf.square(var - init_var)))
