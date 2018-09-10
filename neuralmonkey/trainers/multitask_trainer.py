ac3a5a8d25287f77a95135378d05813cead29ec4
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2018-09-10 09:57:25 +0200

    simplify multitask trainer and reuse variables in generic trainer

--- neuralmonkey/trainers/multitask_trainer.py
+++ neuralmonkey/trainers/multitask_trainer.py
@@ -4,27 +4,20 @@ import tensorflow as tf
 
 from neuralmonkey.runners.base_runner import Executable
 from neuralmonkey.trainers.generic_trainer import GenericTrainer, Objective
-from neuralmonkey.trainers.regularizers import Regularizer
 
 
 # pylint: disable=too-few-public-methods
 class MultitaskTrainer(GenericTrainer):
 
     def __init__(self,
-                 objectives: List[Objective] = None,
-                 clip_norm: float = None,
-                 optimizer: tf.train.Optimizer = None,
-                 regularizers: List[Regularizer] = None,
-                 trainers: List[GenericTrainer] = None,
-                 var_scopes: List[str] = None,
-                 var_collection: str = None) -> None:
+                 trainers: List[GenericTrainer] = None):
         self.trainers = trainers
         self.trainer_idx = len(self.trainers)
 
         # TODO: everything except trainers is ignored now,
         # find a better solution
 
-        self.var_list = list(set([var for t in trainers for var in t.var_list]))
+        self.var_list = list(set().union(*[t.var_list for t in trainers]))
         self.all_coders = set.union(*[t.all_coders for t in self.trainers])
 
     def get_executable(
@@ -35,4 +28,3 @@ class MultitaskTrainer(GenericTrainer):
 
         return self.trainers[self.trainer_idx].get_executable(
             compute_losses, summaries, num_sessions)
-
