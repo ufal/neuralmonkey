eaef1decf5b636f7c9bf4d7575e602a55bce1052
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2018-10-23 14:27:42 +0200

    adhoc semisup fixes

--- neuralmonkey/learning_utils.py
+++ neuralmonkey/learning_utils.py
@@ -231,13 +231,13 @@ def training_loop(tf_manager: TensorFlowManager,
                 if _is_logging_time(log_period_batch, log_period_time,
                                     last_log_time):
                     train_results, train_outputs = run_on_dataset(
-                        tf_manager, runners, batch, postprocess,
-                        write_out=False, batch_size=len(batch))
+                        tf_manager, runners, batch_dataset, postprocess,
+                        write_out=False, batch_size=runners_batch_size)
                     # ensure train outputs are iterable more than once
                     train_outputs = {
                         k: list(v) for k, v in train_outputs.items()}
                     train_evaluation = evaluation(
-                        evaluators, batch, runners, train_results,
+                        evaluators, batch_dataset, runners, train_results,
                         train_outputs)
 
                     _log_continuous_evaluation(
