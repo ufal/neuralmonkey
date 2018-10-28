a5702bea0201ca1fc7b1017ba49b332074f50640
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2018-10-28 16:54:04 +0100

    polishing up multitask trainer code

--- neuralmonkey/trainers/multitask_trainer.py
+++ neuralmonkey/trainers/multitask_trainer.py
@@ -1,5 +1,7 @@
 from typing import Any, List
 
+from typeguard import check_argument_types
+
 from neuralmonkey.runners.base_runner import Executable
 from neuralmonkey.trainers.generic_trainer import GenericTrainer
 
@@ -15,18 +17,17 @@ class MultitaskTrainer:
 
     def __init__(self,
                  trainers: List[GenericTrainer]) -> None:
-        assert trainers is not None
+        check_argument_types()
 
         self.trainers = trainers
         self.trainer_idx = len(trainers)
 
-        self.var_list = list(set().union(*[t.var_list for t in trainers])) \
-            # type: List[Any]
+        self.var_list = list(set.union(*[set(t.var_list) for t in trainers]))
         self.all_coders = set.union(*[t.all_coders for t in self.trainers])
 
     def get_executable(
-            self, compute_losses=True, summaries=True,
-            num_sessions=1) -> Executable:
+            self, compute_losses: bool = True, summaries: bool = True,
+            num_sessions: int = 1) -> Executable:
 
         self.trainer_idx = (self.trainer_idx + 1) % len(self.trainers)
 
