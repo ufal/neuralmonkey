9eae5ecc89746458f0cd722d2c8e8988cae6c5d3
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2018-11-08 14:55:17 +0100

    TemporalStatefulProjection is now a ModelPart

--- neuralmonkey/nn/projection.py
+++ neuralmonkey/nn/projection.py
@@ -1,14 +1,27 @@
 """Module which implements various types of projections."""
-from typing import List, Callable
+from typing import List, Callable, Set
 import tensorflow as tf
+from typeguard import check_argument_types
 from neuralmonkey.nn.utils import dropout
 from neuralmonkey.decorators import tensor
 from neuralmonkey.model.stateful import TemporalStateful
+from neuralmonkey.model.model_part import ModelPart, InitializerSpecs
 
 
-class TemporalSatefulProjection(TemporalStateful):
+class TemporalStatefulProjection(ModelPart, TemporalStateful):
 
-    def __init__(self, inputs: List[TemporalStateful], dim: int) -> None:
+    def __init__(self,
+                 name: str,
+                 inputs: List[TemporalStateful],
+                 dim: int,
+                 reuse: ModelPart = None,
+                 save_checkpoint: str = None,
+                 load_checkpoint: str = None,
+                 initializers: InitializerSpecs = None) -> None:
+        check_argument_types()
+        ModelPart.__init__(self, name, reuse, save_checkpoint, load_checkpoint,
+                           initializers)
+
         self.inputs = inputs
         self.dim = dim
 
@@ -17,20 +30,27 @@ class TemporalSatefulProjection(TemporalStateful):
 
     @tensor
     def assertions(self) -> tf.Tensor:
-        first_mask = inputs[0].temporal_mask
+        first_mask = self.inputs[0].temporal_mask
         return [tf.assert_equal(inp.temporal_mask, first_mask)
                 for inp in self.inputs[1:]]
 
     @tensor
     def temporal_states(self) -> tf.Tensor:
         with tf.control_dependencies(self.assertions):
-            cat = tf.concat([i.temporal_mask for i in self.inputs], axis=2)
+            cat = tf.concat([i.temporal_states for i in self.inputs], axis=2)
             return tf.layers.dense(cat, self.dim)
 
     @tensor
     def temporal_mask(self) -> tf.Tensor:
         with tf.control_dependencies(self.assertions):
             return self.inputs[0].temporal_mask
+
+    def get_dependencies(self) -> Set[ModelPart]:
+        to_return = ModelPart.get_dependencies(self)
+        to_return = to_return.union(
+            *(inp.get_dependencies() for inp in self.inputs))
+
+        return to_return
 
 
 def maxout(inputs: tf.Tensor,
