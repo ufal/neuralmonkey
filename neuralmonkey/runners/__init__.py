7e12bda37177bb1c82659f981fa97089ea680c6b
varisd <varis@ufal.mff.cuni.cz>
2019-03-14 15:47:24 +0100

    removed perplexity runner

--- neuralmonkey/runners/__init__.py
+++ neuralmonkey/runners/__init__.py
@@ -2,7 +2,6 @@ from .beamsearch_runner import BeamSearchRunner
 from .beamsearch_runner import beam_search_runner_range
 from .label_runner import LabelRunner
 from .logits_runner import LogitsRunner
-from .perplexity_runner import PerplexityRunner
 from .plain_runner import PlainRunner
 from .regression_runner import RegressionRunner
 from .runner import GreedyRunner
