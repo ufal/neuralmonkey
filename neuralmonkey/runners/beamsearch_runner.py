2b3ffb7174a6ef792fe16642438465861fc48b50
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2019-07-16 01:27:22 +0200

    upgrading code to new scipy, fixing pydocstyle on 3.0.0 because there is a bug in 4.0.0

--- neuralmonkey/runners/beamsearch_runner.py
+++ neuralmonkey/runners/beamsearch_runner.py
@@ -51,7 +51,7 @@ class BeamSearchRunner(BaseRunner[BeamSearchDecoder]):
                              for res in results]
 
             # Arithmetic mean
-            ens_logprobs = (scipy.misc.logsumexp(prev_logprobs, 0)
+            ens_logprobs = (scipy.special.logsumexp(prev_logprobs, 0)
                             - np.log(self.num_sessions))
 
             if self._is_finished(results):
